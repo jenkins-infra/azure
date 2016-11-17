996e6214618e31015b6ec1f1d06e2c220e9d12e7
R. Tyler Croy <tyler@monkeypox.org>
2016-11-17 12:27:12 -0800

    Refactor the azurerm provider configuration into its own .tf file

--- plans/releases-storage.tf
+++ plans/releases-storage.tf
@@ -5,14 +5,6 @@
 # These resources were originally created manually via the Azure Portal, but
 # this plan represents the enforcement of those resources.
 
-provider "azurerm" {
-    subscription_id = "${var.subscription_id}"
-    client_id       = "${var.client_id}"
-    client_secret   = "${var.client_secret}"
-    tenant_id       = "${var.tenant_id}"
-}
-
-
 resource "azurerm_resource_group" "releases" {
     name     = "${var.prefix}jenkinsinfra-releases"
     location = "East US 2"
