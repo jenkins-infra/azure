00eb18e1e8407f8a3b2a5a099ae1e5ecc21c0f53
Vernin Olivier <olivier.vernin@realimpactanalytics.com>
2016-12-22 09:52:48 +0100

    Update variables.tf
    Remove unneeded white space

--- plans/variables.tf
+++ plans/variables.tf
@@ -7,7 +7,6 @@ variable "location" {
     type    = "string"
     default = "East US"
 }
-
 # Port used for Puppet agents to connect to a Puppet master
 variable "puppet_master_port" {
     default = 8140
