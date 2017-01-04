574570b1fbc77a0dd94742e4e255d6c26ae92d62
olblak <olivier@vernin.me>
2017-01-04 16:37:35 +0100

    Define specific location for logs

--- plans/logs.tf
+++ plans/logs.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "logs" {
     name     = "${var.prefix}logs"
-    location = "${var.location}"
+    location = "${var.logslocation}"
     tags {
         env = "${var.prefix}"
     }
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "logs" {
 resource "azurerm_storage_account" "logs" {
     name                = "${var.prefix}logs"
     resource_group_name = "${azurerm_resource_group.logs.name}"
-    location            = "${var.location}"
+    location            = "${var.logslocation}"
     account_type        = "Standard_GRS"
     depends_on          = ["azurerm_resource_group.logs"]
     tags {
@@ -23,7 +23,7 @@ resource "azurerm_template_deployment" "logs"{
     depends_on          = ["azurerm_resource_group.logs"]
     parameters = {
         omsWorkspaceName = "${var.prefix}logs"
-        omsRegion = "${var.location}"
+        omsRegion = "${var.logslocation}"
         existingStorageAccountName = "${azurerm_storage_account.logs.name}"
         existingStorageAccountResourceGroupName = "${azurerm_resource_group.logs.name}"
         table = "WADWindowsEventLogsTable"
