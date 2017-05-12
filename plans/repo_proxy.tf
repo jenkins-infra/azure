c04e05aa9a2c27aed6fdb30b030a178da0132523
olblak <olivier@vernin.me>
2017-05-12 16:41:17 +0200

    Rename repo-proxy resources

--- plans/repo_proxy.tf
+++ plans/repo_proxy.tf
@@ -3,29 +3,29 @@
 #
 # See: https://issues.jenkins-ci.org/browse/INFRA-1176
 
-resource "azurerm_resource_group" "proxy" {
-    name     = "${var.prefix}proxy"
+resource "azurerm_resource_group" "repo-proxy" {
+    name     = "${var.prefix}repo-proxy"
     location = "${var.location}"
     tags {
         env = "${var.prefix}"
     }
 }
 
-resource "azurerm_storage_account" "proxy" {
-    name                = "${var.prefix}jenkinsproxy"
-    resource_group_name = "${azurerm_resource_group.proxy.name}"
+resource "azurerm_storage_account" "repo-proxy" {
+    name                = "${var.prefix}repoproxy"
+    resource_group_name = "${azurerm_resource_group.repo-proxy.name}"
     location            = "${var.location}"
     account_type        = "Standard_GRS"
-    depends_on          = ["azurerm_resource_group.proxy"]
+    depends_on          = ["azurerm_resource_group.repo-proxy"]
     tags {
         env = "${var.prefix}"
     }
 }
 
-resource "azurerm_storage_share" "repo" {
-    name = "repo"
-    resource_group_name     = "${azurerm_resource_group.proxy.name}"
-    storage_account_name    = "${azurerm_storage_account.proxy.name}"
+resource "azurerm_storage_share" "repo-proxy" {
+    name = "repo-proxy"
+    resource_group_name     = "${azurerm_resource_group.repo-proxy.name}"
+    storage_account_name    = "${azurerm_storage_account.repo-proxy.name}"
     quota                   = 200
-    depends_on              = ["azurerm_resource_group.proxy","azurerm_storage_account.proxy"]
+    depends_on              = ["azurerm_resource_group.repo-proxy","azurerm_storage_account.repo-proxy"]
 }
