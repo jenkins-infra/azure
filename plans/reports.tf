1948f7cd181b049f7866a88c301688bbd00c6af7
olblak <olivier@vernin.me>
2018-01-11 15:26:37 +0100

    Split reports storage account definition

--- plans/reports.tf
+++ plans/reports.tf
@@ -9,7 +9,15 @@ resource "azurerm_resource_group" "reports" {
     location = "${var.location}"
 }
 
-resource "azurerm_storage_account" "reports" {
+#
+# Because the argument 'custom_domain' expects a valide CNAME 'custom_domain' set to <storage_account_name>.blob.core.windows.net and
+# we don't have it for non production environment.
+# And also because Terraform doesn't provide a conditionnal syntax, we choose to create two different storage account', 'report' and 'custom_reports'
+# depending on the environment
+#
+
+resource "azurerm_storage_account" "custom_reports" {
+    count                    = "${ var.prefix == "prod" ? 1 : 0 }"
     name                     = "${var.prefix}jenkinsreports"
     resource_group_name      = "${azurerm_resource_group.reports.name}"
 
@@ -22,9 +30,19 @@ resource "azurerm_storage_account" "reports" {
     }
 }
 
+resource "azurerm_storage_account" "reports" {
+    count                    = "${ var.prefix == "prod" ? 0 : 1 }"
+    name                     = "${var.prefix}jenkinsreports"
+    resource_group_name      = "${azurerm_resource_group.reports.name}"
+
+    location                 = "${var.location}"
+    account_tier             = "Standard"
+    account_replication_type = "GRS"
+}
+
 resource "azurerm_storage_container" "reports" {
     name                  = "reports"
     resource_group_name   = "${azurerm_resource_group.reports.name}"
-    storage_account_name  = "${azurerm_storage_account.reports.name}"
+    storage_account_name  = "${var.prefix}jenkinsreports"
     container_access_type = "container"
 }
