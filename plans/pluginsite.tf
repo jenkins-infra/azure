2b8d7302bf62900d401cb697d2e4e1f37c6252b7
Gavin Mogan <git@gavinmogan.com>
2020-01-09 23:57:04 -0800

    pluginshtml => pluginsite

---
+++ plans/pluginsite.tf
@@ -0,0 +1,32 @@
+#
+# This terraform plan defines the resources necessary to host plugins.jenkins.io
+#
+
+resource "azurerm_resource_group" "pluginsite" {
+  name     = "${var.prefix}pluginsite"
+  location = "${var.location}"
+
+  tags {
+    env = "${var.prefix}"
+  }
+}
+
+resource "azurerm_storage_account" "pluginsite" {
+  name                     = "${var.prefix}pluginsite"
+  resource_group_name      = "${azurerm_resource_group.pluginsite.name}"
+  location                 = "${var.location}"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  depends_on               = ["azurerm_resource_group.pluginsite"]
+
+  tags {
+    env = "${var.prefix}"
+  }
+}
+
+resource "azurerm_storage_share" "pluginsite" {
+  name                 = "pluginsite"
+  resource_group_name  = "${azurerm_resource_group.pluginsite.name}"
+  storage_account_name = "${azurerm_storage_account.pluginsite.name}"
+  depends_on           = ["azurerm_resource_group.pluginsite", "azurerm_storage_account.pluginsite"]
+}
