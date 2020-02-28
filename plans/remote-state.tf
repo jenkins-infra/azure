db100974f8fd262cab633302cebcba925c2fdb7a
Olblak <me@olblak.com>
2020-02-28 14:44:02 +0100

    Fix formatting issue

--- plans/remote-state.tf
+++ plans/remote-state.tf
@@ -26,7 +26,7 @@ resource "azurerm_storage_container" "tfstate" {
 }
 
 output "tfstate_primary_access_key" {
-  value = azurerm_storage_account.tfstate.primary_access_key
+  value     = azurerm_storage_account.tfstate.primary_access_key
   sensitive = true
 }
 
