340146e70100e37eb8c4e6114446f8ea76de0e78
Damien Duportal <damien.duportal@gmail.com>
2022-03-16 15:41:50 +0100

    breaking: re-boostrap the repository on a clean state with the 2022 terraform tooling
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- backend.tf
+++ backend.tf
@@ -1,14 +1,4 @@
-# Terraform 0.9 is designed to find all data it needs at the root of the project and 
-# do not yet support something like 'terraform init plan_directory'.
-# This feature is introduced in v 0.10
-
-# In order to keep the project architecture as is, we just add a second backend declaration
-# at the root of the project and we'll delete this file once we upgrade to 0.10
-
-# doc: https://www.terraform.io/docs/backends/config.html
-# issue: https://github.com/hashicorp/terraform/issues/14066
-# https://github.com/hashicorp/terraform/blob/master/CHANGELOG.md#0100-beta1-june-22-2017
-
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+  }
 }
