a8560275647c131c198ac299a6acdff17ce5db46
smerle33 <smerle@cloudbees.com>
2022-03-28 14:46:12 +0200

    add the output for dbconfig secrets

--- rating.jenkins.io.tf
+++ rating.jenkins.io.tf
@@ -12,3 +12,16 @@ resource "postgresql_role" "rating" {
   login    = true
   password = random_password.pgsql_rating_user_password.result
 }
+
+output "dbconfig" {
+  sensitive   = true
+  description = "file /config/dbconfig.php"
+  value       = <<-EOT
+    <?php
+      $dbuser='${local.public_pgsql_admin_login}';
+      $dbpass='${random_password.pgsql_rating_user_password.result}';
+      $dbname='rating';
+      $dbserver='${azurerm_postgresql_flexible_server.public.fqdn}';
+      $dbport='';
+  EOT
+}
