70751ca3ab6b4e30fc44345ae60435ff5392e4f3
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2022-03-28 17:21:02 +0200

    Fix/pgsql (#184)
    * add private link vnet to public-pgsql
    
    * add superuser false

--- providers.tf
+++ providers.tf
@@ -5,8 +5,9 @@ provider "azurerm" {
 
 provider "postgresql" {
   # Configuration options
-  host     = azurerm_postgresql_flexible_server.public.fqdn
-  username = local.public_pgsql_admin_login
-  password = random_password.pgsql_admin_password.result
+  host      = azurerm_postgresql_flexible_server.public.fqdn
+  username  = local.public_pgsql_admin_login
+  password  = random_password.pgsql_admin_password.result
+  superuser = false
 }
 
--- vnets.tf
+++ vnets.tf
@@ -27,12 +27,21 @@ data "azurerm_resource_group" "public_prod" {
   name = "prod-jenkins-public-prod"
 }
 
+data "azurerm_resource_group" "private_prod" {
+  name = "prod-jenkins-private-prod"
+}
+
 ################################################################################
 ## VIRTUAL NETWORKS
 ################################################################################
 data "azurerm_virtual_network" "public_prod" {
   name                = "prod-jenkins-public-prod"
   resource_group_name = data.azurerm_resource_group.public_prod.name
+}
+
+data "azurerm_virtual_network" "private_prod" {
+  name                = "prod-jenkins-private-prod-vnet"
+  resource_group_name = data.azurerm_resource_group.private_prod.name
 }
 
 ################################################################################
@@ -59,9 +68,16 @@ resource "azurerm_subnet_network_security_group_association" "public_pgsql" {
   network_security_group_id = azurerm_network_security_group.public_pgsql_tier.id
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "public_pgsql" {
-  name                  = "public-pgsql"
+resource "azurerm_private_dns_zone_virtual_network_link" "publicvnet_to_publicpgsql" {
+  name                  = "publicvnet-to-publicpgsql"
   resource_group_name   = data.azurerm_resource_group.public_prod.name
   private_dns_zone_name = azurerm_private_dns_zone.public_pgsql.name
   virtual_network_id    = data.azurerm_virtual_network.public_prod.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "privatevnet_to_publicpgsql" {
+  name                  = "privatevnet-to-publicpgsql"
+  resource_group_name   = data.azurerm_resource_group.private_prod.name
+  private_dns_zone_name = azurerm_private_dns_zone.public_pgsql.name
+  virtual_network_id    = data.azurerm_virtual_network.private_prod.id
 }
