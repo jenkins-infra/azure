6a4a712d1bb22cd8366606f78306532a1e653f05
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2022-03-29 09:12:58 +0200

    add the output for dbconfig secrets (#181)
    Co-authored-by: Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- rating.jenkins.io.tf
+++ rating.jenkins.io.tf
@@ -12,3 +12,17 @@ resource "postgresql_role" "rating" {
   login    = true
   password = random_password.pgsql_rating_user_password.result
 }
+
+# This (sensitive) output is meant to be encrypted into the production secret system, to be provided as a secret to the ratings.jenkins.io application
+output "rating_dbconfig" {
+  sensitive   = true
+  description = "file /config/dbconfig.php"
+  value       = <<-EOT
+    <?php
+      $dbuser='${local.public_pgsql_admin_login}';
+      $dbpass='${random_password.pgsql_rating_user_password.result}';
+      $dbname='rating';
+      $dbserver='${azurerm_postgresql_flexible_server.public.fqdn}';
+      $dbport='';
+  EOT
+}
