38ba8021c6f1d89ee2285b91ab2ee46beea6e089
Damien Duportal <damien.duportal@gmail.com>
2022-09-29 17:01:05 -0400

    feat(reports.jenkins.io) track reports.jenkins.io bucket
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ reports.jenkins.io.tf
@@ -0,0 +1,29 @@
+## This file contains the resources associated to the buckets used to store private and public reports
+resource "azurerm_resource_group" "prod_reports" {
+  name     = "prod-reports"
+  location = "East US 2"
+
+  tags = {
+    scope = "terraform-managed"
+  }
+}
+
+resource "azurerm_storage_account" "prodjenkinsreports" {
+  name                      = "prodjenkinsreports"
+  resource_group_name       = azurerm_resource_group.prod_reports.name
+  location                  = azurerm_resource_group.prod_reports.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  account_kind              = "Storage" # StorageV2
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
+
+  custom_domain {
+    name          = "reports.jenkins.io"
+    use_subdomain = false
+  }
+
+  tags = {
+    scope = "terraform-managed"
+  }
+}
