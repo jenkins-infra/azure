c9b884e890f198a3ea483163a85f3267aac69876
Hervé Le Meur <hlemeur@cloudbees.com>
2022-11-24 17:45:30 +0100

    chore: use new private vnet for privatek8s cluster

--- vnets.tf
+++ vnets.tf
@@ -1,56 +1,53 @@
-#
-# This terraform plan defines the resources necessary to provision the Virtual
-# Networks in Azure according to IEP-002:
-#   <https://github.com/jenkins-infra/iep/tree/master/iep-002>
-#
-#                        +---------------------+
-#                        |                     |
-#      +---------------> |  Public Production  <-------+
-#      |                 |                     |       |
-#      |                 +---------------------+     VNet Peering
-#      |                                               |
-#      |                                 +-------------v--------+
-#                        +-------------+ |                      |
-# The Internet --------> + VPN Gateway |-|  Private Production  |
-#                        +-------------+ |                      |
-#      |                                 +----------------------+
-#      |
-#      |                 +----------------+
-#      |                 |                |
-#      +---------------> |   Development  |
-#                        |                |
-#                        +----------------+
-#
-#
-# See also https://github.com/jenkins-infra/azure/blob/legacy-tf/plans/vnets.tf
+# The resources groups and virtual networks below are defined here:
+# https://github.com/jenkins-infra/azure-net/blob/main/vnets.tf
 
-## RESOURCE GROUPS
-################################################################################
+## Resource Groups
+# Deprecation notice: not included in https://github.com/jenkins-infra/azure-net
 data "azurerm_resource_group" "public_prod" {
   name = "prod-jenkins-public-prod"
 }
 
+# Deprecation notice: not included in https://github.com/jenkins-infra/azure-net
 data "azurerm_resource_group" "private_prod" {
   name = "prod-jenkins-private-prod"
 }
 
-################################################################################
-## VIRTUAL NETWORKS
-################################################################################
+data "azurerm_resource_group" "prod_public" {
+  name = "prod-jenkins-public"
+}
+
+data "azurerm_resource_group" "prod_private" {
+  name = "prod-jenkins-private"
+}
+
+## Virtual Networks
+# Deprecation notice: not included in https://github.com/jenkins-infra/azure-net
 data "azurerm_virtual_network" "public_prod" {
   name                = "prod-jenkins-public-prod"
   resource_group_name = data.azurerm_resource_group.public_prod.name
 }
 
+# Deprecation notice: not included in https://github.com/jenkins-infra/azure-net
 data "azurerm_virtual_network" "private_prod" {
   name                = "prod-jenkins-private-prod-vnet"
   resource_group_name = data.azurerm_resource_group.private_prod.name
+}
+
+data "azurerm_virtual_network" "prod_public" {
+  name                = "prod-jenkins-public-vnet"
+  resource_group_name = data.azurerm_resource_group.prod_public.name
+}
+
+data "azurerm_virtual_network" "prod_private" {
+  name                = "prod-jenkins-private-vnet"
+  resource_group_name = data.azurerm_resource_group.prod_private.name
 }
 
 ################################################################################
 ## SUB NETWORKS
 ################################################################################
 
+# TODO: move into the public vnet defined in https://github.com/jenkins-infra/azure-net
 # "pgsql-tier" subnet is reserved as "delegated" for the pgsql server on the public network
 # Ref. https://docs.microsoft.com/en-us/azure/postgresql/flexible-server/concepts-networking
 resource "azurerm_subnet" "pgsql_tier" {
