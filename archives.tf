0eb7f5c5a4df7556f786f27d68a2d6de39151d18
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-12-02 15:27:05 +0100

    fix(archives): unique storage account name (#241)
    > The storage account named archives is already taken.

--- archives.tf
+++ archives.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "archives" {
 }
 
 resource "azurerm_storage_account" "archives" {
-  name                            = "archives"
+  name                            = "jenkinsinfraarchives"
   resource_group_name             = azurerm_resource_group.archives.name
   location                        = azurerm_resource_group.archives.location
   account_tier                    = "Standard"
