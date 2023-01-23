6670dada11aa231c61be794c739e587ad08c8ada
Hervé Le Meur <hlemeur@cloudbees.com>
2023-01-23 19:05:02 +0100

    fix: subnet in public-vnet

--- publick8s.tf
+++ publick8s.tf
@@ -13,8 +13,8 @@ resource "random_pet" "suffix_publick8s" {
 # https://github.com/jenkins-infra/terraform-states/blob/1f44cdb8c6837021b1007fef383207703b0f4d76/azure/main.tf#L49
 data "azurerm_subnet" "publick8s_tier" {
   name                 = "publick8s-tier"
-  resource_group_name  = data.azurerm_resource_group.private.name
-  virtual_network_name = data.azurerm_virtual_network.private.name
+  resource_group_name  = data.azurerm_resource_group.public.name
+  virtual_network_name = data.azurerm_virtual_network.public.name
 }
 
 #tfsec:ignore:azure-container-logging #tfsec:ignore:azure-container-limit-authorized-ips
@@ -68,40 +68,26 @@ resource "azurerm_kubernetes_cluster_node_pool" "publicpool" {
   max_count             = 20
   zones                 = [3]
   vnet_subnet_id        = data.azurerm_subnet.publick8s_tier.id
-  tags                  = local.default_tags
+
+  #   # Spot instances
+  #   priority        = "Spot"
+  #   eviction_policy = "Delete"
+  #   spot_max_price  = "-1" # in $, -1 = On demand pricing
+  #   # Note: label and taint added automatically when in "Spot" priority, putting it here to explicit them
+  #   node_labels = {
+  #     "kubernetes.azure.com/scalesetpriority" = "spot"
+  #   }
+  #   node_taints = [
+  #     "jenkins=infra.ci.jenkins.io:NoSchedule",
+  #     "kubernetes.azure.com/scalesetpriority=spot:NoSchedule",
+  #   ]
+
+  tags = local.default_tags
 }
-
-# resource "azurerm_kubernetes_cluster_node_pool" "infracipool" {
-#   name                  = "infracipool"
-#   vm_size               = "Standard_D8s_v3"
-#   os_disk_type          = "Ephemeral"
-#   os_disk_size_gb       = 30
-#   kubernetes_cluster_id = azurerm_kubernetes_cluster.privatek8s.id
-#   enable_auto_scaling   = true
-#   min_count             = 0
-#   max_count             = 20
-#   zones                 = [3]
-#   vnet_subnet_id        = data.azurerm_subnet.privatek8s_tier.id
-
-#   # Spot instances
-#   priority        = "Spot"
-#   eviction_policy = "Delete"
-#   spot_max_price  = "-1" # in $, -1 = On demand pricing
-#   # Note: label and taint added automatically when in "Spot" priority, putting it here to explicit them
-#   node_labels = {
-#     "kubernetes.azure.com/scalesetpriority" = "spot"
-#   }
-#   node_taints = [
-#     "jenkins=infra.ci.jenkins.io:NoSchedule",
-#     "kubernetes.azure.com/scalesetpriority=spot:NoSchedule",
-#   ]
-
-#   tags = local.default_tags
-# }
 
 # Allow cluster to manage LBs in the publick8s-tier subnet (Public LB)
 resource "azurerm_role_assignment" "publick8s_networkcontributor" {
-  scope                            = "${data.azurerm_subscription.jenkins.id}/resourceGroups/${data.azurerm_resource_group.private.name}/providers/Microsoft.Network/virtualNetworks/${data.azurerm_virtual_network.private.name}/subnets/${data.azurerm_subnet.publick8s_tier.name}"
+  scope                            = "${data.azurerm_subscription.jenkins.id}/resourceGroups/${data.azurerm_resource_group.public.name}/providers/Microsoft.Network/virtualNetworks/${data.azurerm_virtual_network.public.name}/subnets/${data.azurerm_subnet.publick8s_tier.name}"
   role_definition_name             = "Network Contributor"
   principal_id                     = azurerm_kubernetes_cluster.publick8s.identity[0].principal_id
   skip_service_principal_aad_check = true
@@ -109,7 +95,7 @@ resource "azurerm_role_assignment" "publick8s_networkcontributor" {
 
 # Allow cluster to manage LBs in the data-tier subnet (internal LBs)
 resource "azurerm_role_assignment" "datatier_networkcontributor_public" {
-  scope                            = "${data.azurerm_subscription.jenkins.id}/resourceGroups/${data.azurerm_resource_group.private.name}/providers/Microsoft.Network/virtualNetworks/${data.azurerm_virtual_network.private.name}/subnets/${data.azurerm_subnet.private_vnet_data_tier.name}" #?
+  scope                            = "${data.azurerm_subscription.jenkins.id}/resourceGroups/${data.azurerm_resource_group.public.name}/providers/Microsoft.Network/virtualNetworks/${data.azurerm_virtual_network.public.name}/subnets/${data.azurerm_subnet.public_vnet_data_tier.name}"
   role_definition_name             = "Network Contributor"
   principal_id                     = azurerm_kubernetes_cluster.publick8s.identity[0].principal_id
   skip_service_principal_aad_check = true
