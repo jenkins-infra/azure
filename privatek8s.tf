74650026803b5b9915de53a64ad564137ad7177f
Hervé Le Meur <hlemeur@cloudbees.com>
2023-01-27 15:45:18 +0100

    feat(privatek8s): add highmem and windows node pools needed for release.ci.jenkins.io

--- privatek8s.tf
+++ privatek8s.tf
@@ -101,6 +101,38 @@ resource "azurerm_kubernetes_cluster_node_pool" "infracipool" {
   tags = local.default_tags
 }
 
+resource "azurerm_kubernetes_cluster_node_pool" "highmempool" {
+  name                  = "highmempool"
+  vm_size               = "Standard_D8s_v3"
+  os_disk_type          = "Ephemeral"
+  os_disk_size_gb       = 100
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.privatek8s.id
+  enable_auto_scaling   = true
+  min_count             = 0
+  max_count             = 3
+  zones                 = [3]
+  vnet_subnet_id        = data.azurerm_subnet.privatek8s_tier.id
+
+  tags = local.default_tags
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "windowspool" {
+  name                  = "windowspool"
+  vm_size               = "Standard_D4s_v3"
+  os_disk_type          = "Ephemeral"
+  os_disk_size_gb       = 100
+  os_type               = "Windows"
+  os_sku                = "Windows2019"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.privatek8s.id
+  enable_auto_scaling   = true
+  min_count             = 0
+  max_count             = 3
+  zones                 = [3]
+  vnet_subnet_id        = data.azurerm_subnet.privatek8s_tier.id
+
+  tags = local.default_tags
+}
+
 # Allow cluster to manage LBs in the privatek8s-tier subnet (Public LB)
 resource "azurerm_role_assignment" "privatek8s_networkcontributor" {
   scope                            = "${data.azurerm_subscription.jenkins.id}/resourceGroups/${data.azurerm_resource_group.private.name}/providers/Microsoft.Network/virtualNetworks/${data.azurerm_virtual_network.private.name}/subnets/${data.azurerm_subnet.privatek8s_tier.name}"
