6887a3d426d7230526500da6ab5d59f9b5253e0b
Damien Duportal <damien.duportal@gmail.com>
2023-02-22 08:41:42 +0100

    hotifx(locals) update dduportal main public IP

--- locals.tf
+++ locals.tf
@@ -35,7 +35,7 @@ locals {
   }
 
   admin_allowed_ips = {
-    dduportal   = "109.88.253.125"
+    dduportal   = "85.27.58.68"
     dduportal2  = "149.154.214.236"
     lemeurherve = "176.185.227.180"
     smerle33    = "82.64.5.129"
