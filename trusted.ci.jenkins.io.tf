0fb52c8614bc8196a21f0fd96eec46d2d281027e
Damien Duportal <damien.duportal@gmail.com>
2023-02-28 08:18:44 +0100

    fix(trusted.ci) correct the type of password for the SP
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- trusted.ci.jenkins.io.tf
+++ trusted.ci.jenkins.io.tf
@@ -3,7 +3,7 @@ resource "azuread_application" "trusted_ci_jenkins_io" {
   owners       = [data.azuread_client_config.current.object_id]
   tags         = [for key, value in local.default_tags : "${key}:${value}"]
   required_resource_access {
-    resource_app_id = "00000003-0000-0000-c000-000000000000"  # Microsoft Graph
+    resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
 
     resource_access {
       id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # User.Read
@@ -24,9 +24,9 @@ resource "time_rotating" "trusted_ci_jenkins_io" {
   rotation_days = 365
 }
 
-resource "azuread_application_password" "trusted_ci_jenkins_io" {
-  display_name          = "trusted.ci.jenkins.io-tf-managed"
-  application_object_id = azuread_application.trusted_ci_jenkins_io.id
+resource "azuread_service_principal_password" "trusted_ci_jenkins_io" {
+  display_name         = "trusted.ci.jenkins.io-tf-managed"
+  service_principal_id = azuread_service_principal.trusted_ci_jenkins_io.object_id
   rotate_when_changed = {
     rotation = time_rotating.trusted_ci_jenkins_io.id
   }
