01b96e40acad4a89f0e01058181f855c830f683b
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2023-03-24 16:04:08 +0100

    feat(infra.ci): manage storage account by terraform (#310)
    as per https://github.com/jenkins-infra/helpdesk/issues/3421

--- infra.ci.jenkins.io.tf
+++ infra.ci.jenkins.io.tf
@@ -5,7 +5,15 @@ resource "azurerm_resource_group" "infra_ci_jenkins_io_agents" {
 }
 
 /** Agent Resources **/
-// TODO: create a storage account
+resource "azurerm_storage_account" "infra_ci_jenkins_io_agents" {
+  name                     = "infraciagents"
+  resource_group_name      = azurerm_resource_group.infra_ci_jenkins_io_agents.name
+  location                 = azurerm_resource_group.infra_ci_jenkins_io_agents.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2" # default value, needed for tfsec
+  tags                     = local.default_tags
+}
 // TODO: create a virtual network/subnet for the agent VM
 
 # Azure AD resources to allow controller to spawn agents in Azure
