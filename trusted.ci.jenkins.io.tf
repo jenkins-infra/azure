6f2a974ac643eac8d03ae31c8ebd34d06706e648
smerle33 <smerle@cloudbees.com>
2023-04-25 10:11:20 +0200

    bounce VM only

--- trusted.ci.jenkins.io.tf
+++ trusted.ci.jenkins.io.tf
@@ -30,15 +30,18 @@ data "azurerm_subnet" "trusted_permanent_agents" {
 # Resources groups for TRUSTED Agents
 resource "azurerm_resource_group" "trusted_ci_jenkins_io_agents" {
   name     = "jenkinsinfra-trustedvmagents"
-  location = "East US"
+  location = var.location
+  tags     = local.default_tags
 }
 resource "azurerm_resource_group" "trusted_ci_jenkins_io_permanent_agents" {
   name     = "jenkinsinfra-trusted-permanent-vmagents"
-  location = "East US"
+  location = var.location
+  tags     = local.default_tags
 }
 resource "azurerm_resource_group" "trusted_ci_jenkins_io_controller" {
   name     = "jenkinsinfra-trusted-controller"
-  location = "East US"
+  location = var.location
+  tags     = local.default_tags
 }
 
 #APPLICATION azureAD
@@ -96,70 +99,6 @@ resource "azurerm_role_assignment" "trusted_ci_jenkins_io_allow_packer" {
 # check in vnets.tf
 #
 
-# CONTROLLER VM
-## NETWORK INTERFACE with internal ip
-resource "azurerm_network_interface" "trusted_controller" {
-  name                = "trusted-controller-nic"
-  location            = azurerm_resource_group.trusted_ci_jenkins_io_controller.location
-  resource_group_name = azurerm_resource_group.trusted_ci_jenkins_io_controller.name
-  tags                = local.default_tags
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.trusted_controller.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-## MACHINE (controller)
-resource "azurerm_linux_virtual_machine" "trusted_controller" {
-  name                            = "trusted-controller"
-  resource_group_name             = azurerm_resource_group.trusted_ci_jenkins_io_controller.name
-  location                        = azurerm_resource_group.trusted_ci_jenkins_io_controller.location
-  tags                            = local.default_tags
-  size                            = "Standard_D2as_v5"
-  admin_username                  = "adminuser"
-  disable_password_authentication = true
-  network_interface_ids = [
-    azurerm_network_interface.trusted_controller.id,
-  ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5K7Ro7jBl5Kc68RdzG6EXHstIBFSxO5Da8SQJSMeCbb4cHTYuBBH8jNsAFcnkN64kEu+YhmlxaWEVEIrPgfGfs13ZL7v9p+Nt76tsz6gnVdAy2zCz607pAWe7p4bBn6T9zdZcBSnvjawO+8t/5ue4ngcfAjanN5OsOgLeD6yqVyP8YTERjW78jvp2TFrIYmgWMI5ES1ln32PQmRZwc1eAOsyGJW/YIBdOxaSkZ41qUvb9b3dCorGuCovpSK2EeNphjLPpVX/NRpVY4YlDqAcTCdLdDrEeVqkiA/VDCYNhudZTDa8f1iHwBE/GEtlKmoO6dxJ5LAkRk3RIVHYrmI6XXSw5l0tHhW5D12MNwzUfDxQEzBpGK5iSfOBt5zJ5OiI9ftnsq/GV7vCXfvMVGDLUC551P5/s/wM70QmHwhlGQNLNeJxRTvd6tL11bof3K+29ivFYUmpU17iVxYOWhkNY86WyngHU6Ux0zaczF3H6H0tpg1Ca/cFO428AVPw/RTJpcAe6OVKq5zwARNApQ/p6fJKUAdXap+PpQGZlQhPLkUbwtFXGTrpX9ePTcdzryCYjgrZouvy4ZMzruJiIbFUH8mRY3xVREVaIsJakruvgw3b14oQgcB4BwYVBBqi62xIvbRzAv7Su9t2jK6OR2z3sM/hLJRqIJ5oILMORa7XqrQ== smerle@MacBook-Pro-de-Stephane.local"
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
-    version   = "latest"
-  }
-}
-
-resource "azurerm_managed_disk" "trusted_controller_data_disk" {
-  name                 = "trusted-controller-data-disk"
-  location             = azurerm_resource_group.trusted_ci_jenkins_io_controller.location
-  resource_group_name  = azurerm_resource_group.trusted_ci_jenkins_io_controller.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "100"
-
-  tags = local.default_tags
-}
-
-resource "azurerm_virtual_machine_data_disk_attachment" "trusted_controller_data_disk" {
-  managed_disk_id    = azurerm_managed_disk.trusted_controller_data_disk.id
-  virtual_machine_id = azurerm_linux_virtual_machine.trusted_controller.id
-  lun                = "10"
-  caching            = "ReadWrite"
-}
-
 # BOUNCE VM
 ## PUBLIC IP
 resource "azurerm_public_ip" "trusted_bounce" {
@@ -177,15 +116,10 @@ resource "azurerm_network_interface" "trusted_bounce" {
   tags                = local.default_tags
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.trusted_controller.id
-    private_ip_address_allocation = "Dynamic"
-  }
-  ip_configuration {
     name                          = "external"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.trusted_bounce.id
-    primary                       = true
+    subnet_id                     = data.azurerm_subnet.trusted_controller.id
   }
 }
 ## MACHINE (bounce)
@@ -193,14 +127,13 @@ resource "azurerm_linux_virtual_machine" "trusted_bounce" {
   name                            = "trusted-bounce"
   resource_group_name             = azurerm_resource_group.trusted_ci_jenkins_io_controller.name
   location                        = azurerm_resource_group.trusted_ci_jenkins_io_controller.location
-  size                            = "Standard_F1"
+  size                            = "Standard_B1s"
   admin_username                  = "adminuser"
   disable_password_authentication = true
   network_interface_ids = [
     azurerm_network_interface.trusted_bounce.id,
   ]
 
-  //TODO (check with ddu) add all public ssh keys used today to access the controller
   admin_ssh_key {
     username   = "adminuser"
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5K7Ro7jBl5Kc68RdzG6EXHstIBFSxO5Da8SQJSMeCbb4cHTYuBBH8jNsAFcnkN64kEu+YhmlxaWEVEIrPgfGfs13ZL7v9p+Nt76tsz6gnVdAy2zCz607pAWe7p4bBn6T9zdZcBSnvjawO+8t/5ue4ngcfAjanN5OsOgLeD6yqVyP8YTERjW78jvp2TFrIYmgWMI5ES1ln32PQmRZwc1eAOsyGJW/YIBdOxaSkZ41qUvb9b3dCorGuCovpSK2EeNphjLPpVX/NRpVY4YlDqAcTCdLdDrEeVqkiA/VDCYNhudZTDa8f1iHwBE/GEtlKmoO6dxJ5LAkRk3RIVHYrmI6XXSw5l0tHhW5D12MNwzUfDxQEzBpGK5iSfOBt5zJ5OiI9ftnsq/GV7vCXfvMVGDLUC551P5/s/wM70QmHwhlGQNLNeJxRTvd6tL11bof3K+29ivFYUmpU17iVxYOWhkNY86WyngHU6Ux0zaczF3H6H0tpg1Ca/cFO428AVPw/RTJpcAe6OVKq5zwARNApQ/p6fJKUAdXap+PpQGZlQhPLkUbwtFXGTrpX9ePTcdzryCYjgrZouvy4ZMzruJiIbFUH8mRY3xVREVaIsJakruvgw3b14oQgcB4BwYVBBqi62xIvbRzAv7Su9t2jK6OR2z3sM/hLJRqIJ5oILMORa7XqrQ== smerle@MacBook-Pro-de-Stephane.local"
@@ -209,76 +142,13 @@ resource "azurerm_linux_virtual_machine" "trusted_bounce" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    disk_size_gb         = 32 #minimal size for ubuntu 22.04 image
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
+    offer     = "0001-com-ubuntu-minimal-jammy"
+    sku       = "minimal-22_04-lts-gen2"
     version   = "latest"
   }
-}
-
-# PERMANENT AGENT VM
-## NETWORK INTERFACE with internal ip
-resource "azurerm_network_interface" "trusted_permanent_agent" {
-  name                = "trusted-permanent-agent-nic"
-  location            = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.location
-  resource_group_name = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.name
-  tags                = local.default_tags
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.trusted_permanent_agents.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-## MACHINE (permanent agent)
-resource "azurerm_linux_virtual_machine" "trusted_permanent_agent" {
-  name                            = "trusted-permanent-agents"
-  resource_group_name             = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.name
-  location                        = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.location
-  tags                            = local.default_tags
-  size                            = "Standard_D2as_v5"
-  admin_username                  = "adminuser"
-  disable_password_authentication = true
-  network_interface_ids = [
-    azurerm_network_interface.trusted_permanent_agent.id,
-  ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5K7Ro7jBl5Kc68RdzG6EXHstIBFSxO5Da8SQJSMeCbb4cHTYuBBH8jNsAFcnkN64kEu+YhmlxaWEVEIrPgfGfs13ZL7v9p+Nt76tsz6gnVdAy2zCz607pAWe7p4bBn6T9zdZcBSnvjawO+8t/5ue4ngcfAjanN5OsOgLeD6yqVyP8YTERjW78jvp2TFrIYmgWMI5ES1ln32PQmRZwc1eAOsyGJW/YIBdOxaSkZ41qUvb9b3dCorGuCovpSK2EeNphjLPpVX/NRpVY4YlDqAcTCdLdDrEeVqkiA/VDCYNhudZTDa8f1iHwBE/GEtlKmoO6dxJ5LAkRk3RIVHYrmI6XXSw5l0tHhW5D12MNwzUfDxQEzBpGK5iSfOBt5zJ5OiI9ftnsq/GV7vCXfvMVGDLUC551P5/s/wM70QmHwhlGQNLNeJxRTvd6tL11bof3K+29ivFYUmpU17iVxYOWhkNY86WyngHU6Ux0zaczF3H6H0tpg1Ca/cFO428AVPw/RTJpcAe6OVKq5zwARNApQ/p6fJKUAdXap+PpQGZlQhPLkUbwtFXGTrpX9ePTcdzryCYjgrZouvy4ZMzruJiIbFUH8mRY3xVREVaIsJakruvgw3b14oQgcB4BwYVBBqi62xIvbRzAv7Su9t2jK6OR2z3sM/hLJRqIJ5oILMORa7XqrQ== smerle@MacBook-Pro-de-Stephane.local"
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
-    version   = "latest"
-  }
-}
-
-resource "azurerm_managed_disk" "trusted_permanent_agent_data_disk" {
-  name                 = "trusted-permanent-agent-data-disk"
-  location             = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.location
-  resource_group_name  = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "600"
-
-  tags = local.default_tags
-}
-
-resource "azurerm_virtual_machine_data_disk_attachment" "trusted_permanent_agent_data_disk" {
-  managed_disk_id    = azurerm_managed_disk.trusted_permanent_agent_data_disk.id
-  virtual_machine_id = azurerm_linux_virtual_machine.trusted_permanent_agent.id
-  lun                = "20"
-  caching            = "ReadWrite"
 }
