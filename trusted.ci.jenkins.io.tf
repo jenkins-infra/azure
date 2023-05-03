011cada494dff13d9635d6b08c2fd19c24aed016
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2023-05-03 09:36:19 +0200

    feat(trusted): add controller VM with dynamic ip with dynamic DNS record for this private ip (#338)
    as per https://github.com/jenkins-infra/helpdesk/issues/3486
    
    ---------
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- trusted.ci.jenkins.io.tf
+++ trusted.ci.jenkins.io.tf
@@ -135,9 +135,7 @@ resource "azurerm_network_interface" "trusted_ci_controller" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.trusted_ci_controller.id
-    private_ip_address_allocation = "Static"
-    # TODO add a dns record to point on this private ip
-    private_ip_address = "10.252.0.1" # Manually chosen first IP of the controller subnet
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
