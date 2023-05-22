8a6c37ecbdac5f0799b124d30c9d4f4ed8d8884d
Damien Duportal <damien.duportal@gmail.com>
2023-05-22 18:30:25 +0200

    Revert "feat: add a postgres server" (#359)
    Reverts jenkins-infra/azure#356 because
    https://github.com/jenkins-infra/azure/pull/356#issuecomment-1557524582
    
    We need to check which method to use (with a dedicated private network
    for databases which would be IPv4 only, and the link to create to access
    it from the `public-vnet`)

--- vnets-nsg.tf
+++ vnets-nsg.tf
@@ -1,15 +1,8 @@
 ################################################################################
 ## Public Network
 ################################################################################
-# TODO: remove after migration from prodpublick8s to publick8s is completed (ref: https://github.com/jenkins-infra/helpdesk/issues/3351)
 resource "azurerm_network_security_group" "public_pgsql_tier" {
   name                = "public-network-pgsql-tier"
   location            = var.location
   resource_group_name = data.azurerm_resource_group.public_prod.name
-}
-
-resource "azurerm_network_security_group" "data_pgsql_tier" {
-  name                = "public-network-data-pgsql-tier"
-  location            = var.location
-  resource_group_name = data.azurerm_resource_group.public.name
 }
