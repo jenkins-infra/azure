8c7f8c5a77a23e55d8731aff4157dee6b0b7a62e
Damien Duportal <damien.duportal@gmail.com>
2023-05-22 18:59:29 +0200

    feat(trusted.ci.jenkins.io) allow SSH inbound from the former AWS trusted.ci VM (#358)
    Related to https://github.com/jenkins-infra/helpdesk/issues/3486
    
    This PR allows temporaly the former trusted.ci AWS EC2 VM to reach the
    new bounce.
    The goal is to migrate the data (JENKINS_HOME and agent cache) with
    `rsync`, hence the SSH allowance.
    
    ---------
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- trusted.ci.jenkins.io.tf
+++ trusted.ci.jenkins.io.tf
@@ -555,7 +555,6 @@ resource "azurerm_network_security_rule" "allow_inbound_ssh_from_bounce_to_ephem
 }
 
 resource "azurerm_network_security_rule" "allow_inbound_ssh_from_admins_to_bounce" {
-  # for_each = local.admin_allowed_ips
   name                        = "allow-inbound-ssh-from-admins-to-bounce"
   priority                    = 4000
   direction                   = "Inbound"
@@ -564,6 +563,21 @@ resource "azurerm_network_security_rule" "allow_inbound_ssh_from_admins_to_bounc
   source_port_range           = "*"
   destination_port_range      = "22"
   source_address_prefixes     = values(local.admin_allowed_ips)
+  destination_address_prefix  = azurerm_linux_virtual_machine.trusted_bounce.private_ip_address
+  resource_group_name         = data.azurerm_resource_group.trusted.name
+  network_security_group_name = azurerm_network_security_group.trusted_ci_controller.name
+}
+
+# TODO: remove when data migration is complete
+resource "azurerm_network_security_rule" "allow_inbound_ssh_from_legacy_trusted_to_bounce" {
+  name                        = "allow-inbound-ssh-from-legacy-trusted-to-bounce"
+  priority                    = 4001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefixes     = ["3.209.43.20"]
   destination_address_prefix  = azurerm_linux_virtual_machine.trusted_bounce.private_ip_address
   resource_group_name         = data.azurerm_resource_group.trusted.name
   network_security_group_name = azurerm_network_security_group.trusted_ci_controller.name
