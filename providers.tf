9b9f772c5b9606dfec1027ca2bbcc68c85c98569
Damien Duportal <damien.duportal@gmail.com>
2023-05-24 17:24:51 +0200

    feat(keycloak) manage new DB in public-db
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- providers.tf
+++ providers.tf
@@ -20,17 +20,17 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.publick8s.kube_config.0.cluster_ca_certificate)
 }
 
-# provider "postgresql" {
-#   /**
-#   Important: terraform must be allowed to reach this instance through the network. Check the followings:
-#   - If running in Jenkins, ensure that the subnet of the agents is peered to the subnet of this pgsql instance
-#     * Don't forget to also check the network security group rules
-#   - If running locally, ensure that:
-#     * your /etc/hosts defines an entry with <azurerm_postgresql_flexible_server.public.fqdn> to 127.0.0.1
-#     * you've opened an SSH tunnel such as `ssh -L 5432:<azurerm_postgresql_flexible_server.public.fqdn>:5432` through a machine of the private network
-#   **/
-#   host      = azurerm_postgresql_flexible_server.public.fqdn
-#   username  = local.public_pgsql_admin_login
-#   password  = random_password.pgsql_admin_password.result
-#   superuser = false
-# }
+provider "postgresql" {
+  /**
+  Important: terraform must be allowed to reach this instance through the network. Check the followings:
+  - If running in Jenkins, ensure that the subnet of the agents is peered to the subnet of this pgsql instance
+    * Don't forget to also check the network security group rules
+  - If running locally, ensure that:
+    * your /etc/hosts defines an entry with <azurerm_postgresql_flexible_server.public.fqdn> to 127.0.0.1
+    * you've opened an SSH tunnel such as `ssh -L 5432:<azurerm_postgresql_flexible_server.public.fqdn>:5432` through a machine of the private network
+  **/
+  host      = azurerm_postgresql_flexible_server.public_db.fqdn
+  username  = local.public_db_pgsql_admin_login
+  password  = random_password.public_db_pgsql_admin_password.result
+  superuser = false
+}
