7b9d3c76968bb9d53610cf693dcededfae66bdac
Damien Duportal <damien.duportal@gmail.com>
2023-05-24 18:13:53 +0200

    fixup(postgres) correct output to use the new public-db - fixup of #362
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- keycloak.jenkins.io.tf
+++ keycloak.jenkins.io.tf
@@ -28,7 +28,7 @@ secrets:
             DB_USER: ${base64encode(postgresql_role.keycloak.name)}
             DB_PASSWORD: ${base64encode(random_password.pgsql_keycloak_user_password.result)}
             DB_VENDOR: ${base64encode("postgres")}
-            DB_ADDR: ${base64encode(azurerm_postgresql_flexible_server.public.fqdn)}
+            DB_ADDR: ${base64encode(azurerm_postgresql_flexible_server.public_db.fqdn)}
             DB_PORT: ${base64encode("5432")}
             DB_DATABASE: ${base64encode(postgresql_database.keycloak.name)}
 EOT
