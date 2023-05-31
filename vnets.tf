91196650efbce5d826f182f4eb92f0fe2ce7decc
Damien Duportal <damien.duportal@gmail.com>
2023-05-31 11:14:13 +0200

    cleanup: remove PostgreSQL public instance (in favor of the `public-db` instance) (#372)
    Related to https://github.com/jenkins-infra/helpdesk/issues/3351
    
    The 3 databases (`rating`, `plugin-health-score` and `keycloak`) had
    been migrated to the `public-db` instance so we can delete the `public`
    legacy instance.
    
    It also removes the associated network resources.
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- vnets.tf
+++ vnets.tf
@@ -53,28 +53,3 @@ data "azurerm_subnet" "private_vnet_data_tier" {
   virtual_network_name = data.azurerm_virtual_network.private.name
   resource_group_name  = data.azurerm_resource_group.private.name
 }
-
-# "pgsql-tier" subnet is reserved as "delegated" for the pgsql server on the public network
-# Ref. https://docs.microsoft.com/en-us/azure/postgresql/flexible-server/concepts-networking
-# TODO: remove after migration from prodpublick8s to publick8s is completed (ref: https://github.com/jenkins-infra/helpdesk/issues/3351)
-resource "azurerm_subnet" "pgsql_tier" {
-  name                 = "pgsql-tier"
-  resource_group_name  = data.azurerm_resource_group.public_prod.name
-  virtual_network_name = data.azurerm_virtual_network.public_prod.name
-  address_prefixes     = ["10.0.3.0/24"]
-  delegation {
-    name = "pgsql"
-    service_delegation {
-      name = "Microsoft.DBforPostgreSQL/flexibleServers"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-      ]
-    }
-  }
-}
-
-# TODO: remove after migration from prodpublick8s to publick8s is completed (ref: https://github.com/jenkins-infra/helpdesk/issues/3351)
-resource "azurerm_subnet_network_security_group_association" "public_pgsql" {
-  subnet_id                 = azurerm_subnet.pgsql_tier.id
-  network_security_group_id = azurerm_network_security_group.public_pgsql_tier.id
-}
