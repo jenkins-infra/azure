64a0ab10205086e007206ef3d94b7c623ad72894
Damien Duportal <damien.duportal@gmail.com>
2023-06-05 17:37:22 +0200

    hotfix(ldap) comment all
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- ldap.jenkins.io.tf
+++ ldap.jenkins.io.tf
@@ -4,23 +4,23 @@
 #   tags     = local.default_tags
 # }
 
-resource "azurerm_storage_account" "ldap_backups" {
-  name                     = "ldapjenkinsiobackups"
-  resource_group_name      = azurerm_resource_group.ldap.name
-  location                 = azurerm_resource_group.ldap.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS" # recommended for backups
-  # https://learn.microsoft.com/en-gb/azure/storage/common/infrastructure-encryption-enable
-  infrastructure_encryption_enabled = true
-  min_tls_version                   = "TLS1_2" # default value, needed for tfsec
+# resource "azurerm_storage_account" "ldap_backups" {
+#   name                     = "ldapjenkinsiobackups"
+#   resource_group_name      = azurerm_resource_group.ldap.name
+#   location                 = azurerm_resource_group.ldap.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS" # recommended for backups
+#   # https://learn.microsoft.com/en-gb/azure/storage/common/infrastructure-encryption-enable
+#   infrastructure_encryption_enabled = true
+#   min_tls_version                   = "TLS1_2" # default value, needed for tfsec
 
-  network_rules {
-    default_action             = "Deny"
-    ip_rules                   = values(local.admin_allowed_ips)
-    virtual_network_subnet_ids = [data.azurerm_subnet.publick8s_tier.id]
-    # Grant access to trusted Azure Services like Azure Backup (see # https://learn.microsoft.com/en-gb/azure/storage/common/storage-network-security?tabs=azure-portal#exceptions)
-    bypass = ["AzureServices"]
-  }
+#   network_rules {
+#     default_action             = "Deny"
+#     ip_rules                   = values(local.admin_allowed_ips)
+#     virtual_network_subnet_ids = [data.azurerm_subnet.publick8s_tier.id]
+#     # Grant access to trusted Azure Services like Azure Backup (see # https://learn.microsoft.com/en-gb/azure/storage/common/storage-network-security?tabs=azure-portal#exceptions)
+#     bypass = ["AzureServices"]
+#   }
 
-  tags = local.default_tags
-}
+#   tags = local.default_tags
+# }
