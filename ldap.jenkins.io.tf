2c12cbb6a35533345b20985afc35b30e07fa2f66
Damien Duportal <damien.duportal@gmail.com>
2023-06-05 17:27:05 +0200

    hotfix(ldap) disable ldap backup network rules temporarly
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- ldap.jenkins.io.tf
+++ ldap.jenkins.io.tf
@@ -14,13 +14,13 @@ resource "azurerm_storage_account" "ldap_backups" {
   infrastructure_encryption_enabled = true
   min_tls_version                   = "TLS1_2" # default value, needed for tfsec
 
-  network_rules {
-    default_action             = "Deny"
-    ip_rules                   = values(local.admin_allowed_ips)
-    virtual_network_subnet_ids = [data.azurerm_subnet.publick8s_tier.id]
-    # Grant access to trusted Azure Services like Azure Backup (see # https://learn.microsoft.com/en-gb/azure/storage/common/storage-network-security?tabs=azure-portal#exceptions)
-    bypass = ["AzureServices"]
-  }
+  # network_rules {
+  #   default_action             = "Deny"
+  #   ip_rules                   = values(local.admin_allowed_ips)
+  #   virtual_network_subnet_ids = [data.azurerm_subnet.publick8s_tier.id]
+  #   # Grant access to trusted Azure Services like Azure Backup (see # https://learn.microsoft.com/en-gb/azure/storage/common/storage-network-security?tabs=azure-portal#exceptions)
+  #   bypass = ["AzureServices"]
+  # }
 
   tags = local.default_tags
 }
