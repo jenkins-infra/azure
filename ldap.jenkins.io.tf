4a6497494dab61ba40df753cfc0ef9f1791e510b
Hervé Le Meur <hlemeur@cloudbees.com>
2023-06-06 16:11:00 +0200

    feat(ldap.jenkins.io): add `ldap` File Share to `ldapjenkinsiobackups`

--- ldap.jenkins.io.tf
+++ ldap.jenkins.io.tf
@@ -27,6 +27,22 @@ resource "azurerm_storage_account_network_rules" "ldap_access" {
   bypass = ["AzureServices"]
 }
 
+resource "azurerm_storage_share" "ldap" {
+  name                 = "ldap"
+  storage_account_name = azurerm_storage_account.ldap_backups.name
+  quota                = 5120 # 5To
+
+  acl {
+    id = "MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI"
+
+    access_policy {
+      permissions = "rwdl"
+      start       = "2019-07-02T09:38:21.0000000Z"
+      expiry      = "2019-07-02T10:38:21.0000000Z"
+    }
+  }
+}
+
 output "ldap_backups_primary_access_key" {
   value     = azurerm_storage_account.ldap_backups.primary_access_key
   sensitive = true
