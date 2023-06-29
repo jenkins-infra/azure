0dbdb9972c8cd44dca23062e97dd3c1bf1c75396
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-06-29 15:33:59 +0200

    Revert "hotfix(publick8s) remove AAAA (IPv6) DNS record for `get.jenkins.io` (#417)"
    This reverts commit 5e551f58bdcf55a648accbd0dfe0b5ee3e2e3fbb.

--- publick8s.tf
+++ publick8s.tf
@@ -180,15 +180,14 @@ resource "azurerm_dns_a_record" "public_publick8s" {
   tags                = local.default_tags
 }
 
-## TODO: uncomment for https://github.com/jenkins-infra/helpdesk/issues/3639
-# resource "azurerm_dns_aaaa_record" "public_publick8s" {
-#   name                = "public.publick8s"
-#   zone_name           = data.azurerm_dns_zone.jenkinsio.name
-#   resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
-#   ttl                 = 300
-#   records             = [azurerm_public_ip.publick8s_ipv6.ip_address]
-#   tags                = local.default_tags
-# }
+resource "azurerm_dns_aaaa_record" "public_publick8s" {
+  name                = "public.publick8s"
+  zone_name           = data.azurerm_dns_zone.jenkinsio.name
+  resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
+  ttl                 = 300
+  records             = [azurerm_public_ip.publick8s_ipv6.ip_address]
+  tags                = local.default_tags
+}
 
 resource "azurerm_dns_a_record" "private_publick8s" {
   name                = "private.publick8s"
