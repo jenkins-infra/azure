fb06dcb0e63ddcc6d8f569abad7db41e12bb181d
Damien Duportal <damien.duportal@gmail.com>
2023-07-04 15:48:50 +0200

    hotfix(ci.jenkins.io) fix incorrect outbound NSG rule
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- ci.jenkins.io.tf
+++ ci.jenkins.io.tf
@@ -170,7 +170,7 @@ resource "azurerm_network_security_rule" "deny_all_outbound_from_ci_controller_s
   protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefixes     = "VirtualNetwork"
+  source_address_prefix       = "VirtualNetwork"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.ci_jenkins_io_controller.name
   network_security_group_name = azurerm_network_security_group.ci_jenkins_io_controller.name
