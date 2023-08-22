ea8f31f22edd652e9d3fbb1aeaa1581bdbcf93c8
Damien Duportal <damien.duportal@gmail.com>
2023-08-22 18:25:08 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -41,6 +41,7 @@ locals {
     smerle33         = "82.64.5.129"
     dduportal-cowork = "86.202.255.126"
     dduportal-cell   = "92.184.123.91"
+    dduportal-2      = "88.175.15.95"
   }
 
   external_services = {
