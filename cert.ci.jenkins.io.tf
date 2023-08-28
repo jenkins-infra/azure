58e52440a4ba252afb140064a0667fafebe6d4fc
Damien Duportal <damien.duportal@gmail.com>
2023-08-28 16:22:43 +0200

    feat(cert.ci) use a NAT gateway to ensure scalabale and stable egress system + static outbound IP (#465)
    Related to https://github.com/jenkins-infra/helpdesk/issues/3688
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- cert.ci.jenkins.io.tf
+++ cert.ci.jenkins.io.tf
@@ -6,6 +6,20 @@ data "azurerm_dns_zone" "cert_ci_jenkins_io" {
   name                = "cert.ci.jenkins.io"
   resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
 }
+data "azurerm_virtual_network" "cert_ci_jenkins_io" {
+  name                = "cert-ci-jenkins-io-vnet"
+  resource_group_name = data.azurerm_resource_group.cert_ci_jenkins_io.name
+}
+data "azurerm_subnet" "cert_ci_jenkins_io_controller" {
+  name                 = "cert-ci-jenkins-io-vnet-controller"
+  virtual_network_name = data.azurerm_virtual_network.cert_ci_jenkins_io.name
+  resource_group_name  = data.azurerm_resource_group.cert_ci_jenkins_io.name
+}
+data "azurerm_subnet" "cert_ci_jenkins_io_ephemeral_agents" {
+  name                 = "cert-ci-jenkins-io-vnet-ephemeral-agents"
+  virtual_network_name = data.azurerm_virtual_network.cert_ci_jenkins_io.name
+  resource_group_name  = data.azurerm_resource_group.cert_ci_jenkins_io.name
+}
 
 module "cert_ci_jenkins_io" {
   source = "./.shared-tools/terraform/modules/azure-jenkins-controller"
@@ -14,10 +28,10 @@ module "cert_ci_jenkins_io" {
   location                     = data.azurerm_resource_group.cert_ci_jenkins_io.location
   admin_username               = local.admin_username
   admin_ssh_publickey          = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDDpxwvySus2OWViWfJ02XMYr+Qa/uPADhjt/4el2SmEf7NlJXzq5vc8imcw8YxQZKwuuKJhonlTYTpk1Cjka4bJKWNOSQ8+Kx0O2ZnNjKn3ZETWJB90bZXHVqbrNHDtu6lN6S/yRW9Q+6fuDbHBW0MXWI8Lsv+bU5v8Zll6m62rc00/I/IT9c1TX1qjCtjf5XHMFw7nVxQiTX2Zf5UKG3RI7mkCMDIvx2H9kXdzM8jtYwATZPHKHuLzffARmvy1FpNPVuLLEGYE3hljP82rll1WZbbl1ZrhjzbFUUYO4fsA7AOQHWhHiVLvtnreB269JOl/ZkHgk37zcdwJMkqKpqoEbjP9z8PURf5uMA7TiDGcpgcFMzoaFk1ueqoHM2JaM2AZQAkPhbUfT7MSOFYRx91OEg5pg5N17zNeaBM6fyxl3v7mkxSOTkKlzjAXPRyo7XsosUVQ4qb4DfsAAJ0Rynts2olRQLEzJku0ZxbbXotuoppI8HivRl7PoTsAASJRpc="
-  controller_network_name      = "cert-ci-jenkins-io-vnet"
+  controller_network_name      = data.azurerm_virtual_network.cert_ci_jenkins_io.name
   controller_network_rg_name   = data.azurerm_resource_group.cert_ci_jenkins_io.name
-  controller_subnet_name       = "cert-ci-jenkins-io-vnet-controller"
-  ephemeral_agents_subnet_name = "cert-ci-jenkins-io-vnet-ephemeral-agents"
+  controller_subnet_name       = data.azurerm_subnet.cert_ci_jenkins_io_controller.name
+  ephemeral_agents_subnet_name = data.azurerm_subnet.cert_ci_jenkins_io_ephemeral_agents.name
   controller_data_disk_size_gb = 128
   controller_vm_size           = "Standard_D2as_v5"
   default_tags                 = local.default_tags
@@ -51,6 +65,35 @@ resource "azurerm_dns_a_record" "cert_ci_jenkins_io" {
   resource_group_name = data.azurerm_dns_zone.cert_ci_jenkins_io.resource_group_name
   ttl                 = 60
   records             = [module.cert_ci_jenkins_io.controller_private_ipv4]
+}
+
+####################################################################################
+## NAT gateway to allow outbound connection on a centralized and scalable appliance
+####################################################################################
+resource "azurerm_public_ip" "cert_ci_jenkins_io_outbound" {
+  name                = "cert-ci-jenkins-io-outbound"
+  location            = data.azurerm_resource_group.cert_ci_jenkins_io.location
+  resource_group_name = module.cert_ci_jenkins_io.controller_resourcegroup_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+resource "azurerm_nat_gateway" "cert_ci_jenkins_io_outbound" {
+  name                = "cert-ci-jenkins-io-outbound"
+  location            = data.azurerm_resource_group.cert_ci_jenkins_io.location
+  resource_group_name = module.cert_ci_jenkins_io.controller_resourcegroup_name
+  sku_name            = "Standard"
+}
+resource "azurerm_nat_gateway_public_ip_association" "cert_ci_jenkins_io_outbound" {
+  nat_gateway_id       = azurerm_nat_gateway.cert_ci_jenkins_io_outbound.id
+  public_ip_address_id = azurerm_public_ip.cert_ci_jenkins_io_outbound.id
+}
+resource "azurerm_subnet_nat_gateway_association" "cert_ci_jenkins_io_outbound_controller" {
+  subnet_id      = data.azurerm_subnet.cert_ci_jenkins_io_controller.id
+  nat_gateway_id = azurerm_nat_gateway.cert_ci_jenkins_io_outbound.id
+}
+resource "azurerm_subnet_nat_gateway_association" "cert_ci_jenkins_io_outbound_ephemeral_agents" {
+  subnet_id      = data.azurerm_subnet.cert_ci_jenkins_io_ephemeral_agents.id
+  nat_gateway_id = azurerm_nat_gateway.cert_ci_jenkins_io_outbound.id
 }
 
 ######### Legacy resources (TODO: delete everything below once https://github.com/jenkins-infra/helpdesk/issues/3688 is migrated)
