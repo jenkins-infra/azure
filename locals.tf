4a2e7776e9ba0af3669fcc943ab14cc57c052d03
Damien Duportal <damien.duportal@gmail.com>
2023-09-17 11:15:50 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -35,9 +35,8 @@ locals {
   }
 
   admin_allowed_ips = {
-    dduportal     = "85.27.34.43"   # Home
-    dduportal-2   = "91.182.56.152" # Cowork
-    dduportal-3   = "91.175.63.234" # Cowork2
+    dduportal     = "85.27.34.43"
+    dduportal-2   = "86.202.255.126"
     lemeurherve   = "176.185.227.180"
     lemeurherve-2 = "176.145.123.59"
     smerle33      = "82.64.5.129"
