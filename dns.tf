cbdf98f0080c13027c81fda770d2ee27980995b2
Damien Duportal <damien.duportal@gmail.com>
2023-09-28 15:38:14 +0200

    feat(archives.jenkins.io) import existing CNAME records for archives.jenkins.io and archives.jenkins-ci.org
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- dns.tf
+++ dns.tf
@@ -1,8 +1,17 @@
+# Jenkins.io DNS zone
 data "azurerm_resource_group" "proddns_jenkinsio" {
   name = "proddns_jenkinsio"
 }
-
 data "azurerm_dns_zone" "jenkinsio" {
   name                = "jenkins.io"
   resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
+}
+
+# Jenkins-ci.org DNS zone
+data "azurerm_resource_group" "proddns_jenkinsci" {
+  name = "proddns_jenkinsci"
+}
+data "azurerm_dns_zone" "jenkinsciorg" {
+  name                = "jenkins-ci.org"
+  resource_group_name = data.azurerm_resource_group.proddns_jenkinsci.name
 }
