01d5435292c7d6f4d6f04a39d82ce9bbc720fc9e
Damien Duportal <damien.duportal@gmail.com>
2023-11-07 14:06:03 +0100

    chore(infra.ci) fix depreciation message about azuread_service_principal application_id
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- infra.ci.jenkins.io.tf
+++ infra.ci.jenkins.io.tf
@@ -35,16 +35,16 @@ resource "azuread_application" "infra_ci_jenkins_io" {
   }
 }
 resource "azuread_service_principal" "infra_ci_jenkins_io" {
-  application_id               = azuread_application.infra_ci_jenkins_io.application_id
+  client_id                    = azuread_application.infra_ci_jenkins_io.client_id
   app_role_assignment_required = false
   owners = [
     data.azuread_service_principal.terraform_production.id,
   ]
 }
 resource "azuread_application_password" "infra_ci_jenkins_io" {
-  application_object_id = azuread_application.infra_ci_jenkins_io.object_id
-  display_name          = "infra.ci.jenkins.io-tf-managed"
-  end_date              = "2024-03-22T00:00:00Z"
+  application_id = azuread_application.infra_ci_jenkins_io.id
+  display_name   = "infra.ci.jenkins.io-tf-managed"
+  end_date       = "2024-03-22T00:00:00Z"
 }
 # Allow Service Principal to manage AzureRM resources inside the agents resource groups
 resource "azurerm_role_assignment" "infra_ci_jenkins_io_allow_azurerm" {
