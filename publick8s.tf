dcb0ee9eaeb16f6967f52c9b4e036d3a8a644e77
smerle33 <stephane.merle+jenkins@gmail.com>
2023-12-04 09:26:52 +0100

    feat(publick8s): create a x86small nodepool to migrate from x86medium

--- publick8s.tf
+++ publick8s.tf
@@ -84,6 +84,26 @@ resource "azurerm_kubernetes_cluster" "publick8s" {
   tags = local.default_tags
 }
 
+resource "azurerm_kubernetes_cluster_node_pool" "x86small" {
+  name                  = "x86small"
+  vm_size               = "Standard_D4s_v3" # 4 vCPU, 16 GB RAM, 32 GB disk, 8 000 IOPS
+  os_disk_type          = "Ephemeral"
+  os_disk_size_gb       = 100 # Ref. Cache storage size at https://learn.microsoft.com/en-us/azure/virtual-machines/dv3-dsv3-series#dsv3-series (depends on the instance size)
+  orchestrator_version  = local.kubernetes_versions["publick8s"]
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.publick8s.id
+  enable_auto_scaling   = true
+  min_count             = 0
+  max_count             = 10
+  zones                 = local.publick8s_compute_zones
+  vnet_subnet_id        = data.azurerm_subnet.publick8s_tier.id
+
+  lifecycle {
+    ignore_changes = [node_count]
+  }
+
+  tags = local.default_tags
+}
+
 resource "azurerm_kubernetes_cluster_node_pool" "x86medium" {
   name                  = "x86medium"
   vm_size               = "Standard_D8s_v3" # 8 vCPU, 32 GB RAM, 64 GB disk, 16 000 IOPS
