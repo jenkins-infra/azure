5f80644c7d002e11dca8483f21c87e1f9554c5ca
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2023-12-07 11:06:03 +0100

    feat(publick8s): increase node number to 2 for system pool (#542)
    as per
    https://github.com/jenkins-infra/helpdesk/issues/3827#issuecomment-1842725422

--- publick8s.tf
+++ publick8s.tf
@@ -73,7 +73,7 @@ resource "azurerm_kubernetes_cluster" "publick8s" {
     os_disk_size_gb              = 50
     orchestrator_version         = local.kubernetes_versions["publick8s"]
     enable_auto_scaling          = false
-    node_count                   = 1
+    node_count                   = 2
     vnet_subnet_id               = data.azurerm_subnet.publick8s_tier.id
     tags                         = local.default_tags
     zones                        = local.publick8s_compute_zones
