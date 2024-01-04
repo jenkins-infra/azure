a5e87b7842dd2980857c691c1134d50407ad7330
smerle33 <stephane.merle+jenkins@gmail.com>
2024-01-04 11:39:44 +0100

    chore(updatecli): add expiry manifest with date bash scripts

---
+++ updatecli/scripts/date_next_expiry.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script calculate next date for expiry azurerm_storage_account_sas
+##
+set -eux -o pipefail
+DATE_BIN='date'
+
+## non GNU operating system
+if command -v gdate  >/dev/null 2>&1
+then
+    DATE_BIN='gdate'
+fi
+command -v "${DATE_BIN}" >/dev/null 2>&1 || { echo "ERROR: ${DATE_BIN} command not found. Exiting."; exit 1; }
+
+"${DATE_BIN}" --utc +"%Y-%m-%dT00:00:00Z" -d '+1 month'
---
+++ updatecli/scripts/datediff_expiry.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script calculate diff between dates for expiry azurerm_storage_account_sas
+##
+set -eux -o pipefail
+
+currentexpirydate="${1}"
+DATE_BIN='date'
+
+## non GNU operating system
+if command -v gdate  >/dev/null 2>&1
+then
+    DATE_BIN='gdate'
+fi
+command -v "${DATE_BIN}" >/dev/null 2>&1 || { echo "ERROR: ${DATE_BIN} command not found. Exiting."; exit 1; }
+
+currentdateepoch=$("${DATE_BIN}" --utc "+%s" 2>/dev/null)
+expirydateepoch=$("${DATE_BIN}" "+%s" -d "$currentexpirydate")
+datediff=$(((expirydateepoch-currentdateepoch)/86400))
+
+if [ "$datediff" -lt 10 ]
+then
+    echo "time for update"
+    exit 0
+else
+    echo "not yet expired"
+    exit 1
+fi
