079d8e89568ba72cfd289e2a3a7d3b7722ed26b6
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2024-01-06 08:31:49 +0100

    chore(updatecli): add expiry manifest with date bash scripts (#566)
    as per
    https://github.com/jenkins-infra/helpdesk/issues/3875#issuecomment-1874329296
    
    ---------
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ updatecli/scripts/date_next_expiry.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This script calculate next date for expiry azurerm_storage_account_sas
+##
+set -eux -o pipefail
+DATE_BIN='date'
+
+## non GNU operating system
+if command -v gdate >/dev/null 2>&1
+then
+    DATE_BIN='gdate'
+fi
+command -v "${DATE_BIN}" >/dev/null 2>&1 || { echo "ERROR: ${DATE_BIN} command not found. Exiting."; exit 1; }
+
+# "${DATE_BIN}" --utc +"%Y-%m-%dT00:00:00Z" -d '+1 month' #keep for later use with allinone not alpine based
+"${DATE_BIN}" -d@"$(( $(date +%s)+60*60*24*31))" --utc '+%Y-%m-%dT00:00:00Z'
