9d0d9010912d4464e4bbd64a125ffbecb2c300cf
Damien Duportal <damien.duportal@gmail.com>
2024-01-06 15:08:47 +0100

    hotfix(updates.jenkins.io) restrict Azure SA to only the trusted and infra subnets (instead of NAT gaetway outbound IPs
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- updates.jenkins.io.tf
+++ updates.jenkins.io.tf
@@ -21,11 +21,15 @@ resource "azurerm_storage_account" "updates_jenkins_io" {
     ip_rules = flatten(
       concat(
         [for key, value in module.jenkins_infra_shared_data.admin_public_ips : value],
-        module.jenkins_infra_shared_data.outbound_ips["trusted.ci.jenkins.io"],
-        module.jenkins_infra_shared_data.outbound_ips["trusted.sponsorship.ci.jenkins.io"],
-        module.jenkins_infra_shared_data.outbound_ips["privatek8s.jenkins.io"]
       )
     )
+    virtual_network_subnet_ids = [
+      data.azurerm_subnet.trusted_ci_jenkins_io_ephemeral_agents.id,
+      data.azurerm_subnet.trusted_ci_jenkins_io_permanent_agents.id,
+      data.azurerm_subnet.trusted_ci_jenkins_io_sponsorship_ephemeral_agents.id,
+      data.azurerm_subnet.privatek8s_tier.id,
+      data.azurerm_subnet.infra_ci_jenkins_io_sponsorship_ephemeral_agents.id,
+    ]
     bypass = ["Metrics", "Logging", "AzureServices"]
   }
 }
