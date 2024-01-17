8e1f345ba0f75dca69eae5418c8fcb0d1d55e158
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2024-01-17 09:01:52 +0100

    chore(updatecli): change date detection for expiry and new certificate lifespan (#573)
    as per
    https://github.com/jenkins-infra/helpdesk/issues/3875#issuecomment-1874329296

--- updatecli/scripts/date_next_expiry.sh
+++ updatecli/scripts/date_next_expiry.sh
@@ -12,4 +12,4 @@ fi
 command -v "${DATE_BIN}" >/dev/null 2>&1 || { echo "ERROR: ${DATE_BIN} command not found. Exiting."; exit 1; }
 
 # "${DATE_BIN}" --utc +"%Y-%m-%dT00:00:00Z" -d '+1 month' #keep for later use with allinone not alpine based
-"${DATE_BIN}" -d@"$(( $(date +%s)+60*60*24*31))" --utc '+%Y-%m-%dT00:00:00Z'
+"${DATE_BIN}" -d@"$(( $(date +%s)+60*60*24*30*3))" --utc '+%Y-%m-%dT00:00:00Z' # next expiry in around 3 month
