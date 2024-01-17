0b0264d6c590b85907f87562b617b47ceb520fc3
Damien Duportal <damien.duportal@gmail.com>
2024-01-17 15:33:48 +0100

    Update privatek8s.tf

--- privatek8s.tf
+++ privatek8s.tf
@@ -49,7 +49,7 @@ resource "azurerm_kubernetes_cluster" "privatek8s" {
     outbound_type     = "loadBalancer"
     load_balancer_sku = "standard"
     load_balancer_profile {
-      outbound_ports_allocated  = "1600"
+      outbound_ports_allocated  = "1280" # Max 50 Nodes, 64000 total
       idle_timeout_in_minutes   = "4"
       managed_outbound_ip_count = "1"
     }
