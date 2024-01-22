4c259746678b8f51e5d3445a18a60810fbc5f338
smerle33 <stephane.merle+jenkins@gmail.com>
2024-01-22 16:15:37 +0100

    remove -D as not needed anymore with debian date

--- updatecli/scripts/datediff_expiry.sh
+++ updatecli/scripts/datediff_expiry.sh
@@ -14,7 +14,7 @@ fi
 command -v "${DATE_BIN}" >/dev/null 2>&1 || { echo "ERROR: ${DATE_BIN} command not found. Exiting."; exit 1; }
 
 currentdateepoch=$("${DATE_BIN}" --utc "+%s" 2>/dev/null)
-expirydateepoch=$("${DATE_BIN}" "+%s" -d "$currentexpirydate" -D"%Y-%m-%dT00:00:00Z")
+expirydateepoch=$("${DATE_BIN}" "+%s" -d "$currentexpirydate")
 datediff=$(((expirydateepoch-currentdateepoch)/(60*60*24))) # diff per days
 
 if [ "$datediff" -lt 10 ] # launch renew 10 days before expiration
