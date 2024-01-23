6e5577a360a8c02a29fef6c5dfd9e2cb43d46aeb
smerle33 <stephane.merle+jenkins@gmail.com>
2024-01-23 18:09:09 +0100

    WIP(get.jenkins.io/storage): new premium storage class for get.jenkins.io

---
+++ get.jenkins.io.tf
@@ -0,0 +1,36 @@
+# Storage account
+resource "azurerm_resource_group" "get_jenkins_io" {
+  name     = "get-jenkins-io"
+  location = var.location
+  tags     = local.default_tags
+}
+
+resource "azurerm_storage_account" "get_jenkins_io" {
+  name                     = "getjenkinsio"
+  resource_group_name      = azurerm_resource_group.get_jenkins_io.name
+  location                 = azurerm_resource_group.get_jenkins_io.location
+  account_tier             = "Premium"
+  account_replication_type = "ZRS"
+  min_tls_version          = "TLS1_2" # default value, needed for tfsec
+
+  tags = local.default_tags
+
+  # Adding a network rule with `public_network_access_enabled` set to `true` (default) selects the option "Enabled from selected virtual networks and IP addresses"
+  network_rules {
+    default_action = "Deny"
+    ip_rules = flatten(
+      concat(
+        [for key, value in module.jenkins_infra_shared_data.admin_public_ips : value],
+      )
+    )
+    virtual_network_subnet_ids = [
+      data.azurerm_subnet.trusted_ci_jenkins_io_ephemeral_agents.id,
+      data.azurerm_subnet.trusted_ci_jenkins_io_permanent_agents.id,
+      data.azurerm_subnet.trusted_ci_jenkins_io_sponsorship_ephemeral_agents.id,
+      data.azurerm_subnet.publick8s_tier.id,
+      data.azurerm_subnet.privatek8s_tier.id,
+      data.azurerm_subnet.infra_ci_jenkins_io_sponsorship_ephemeral_agents.id,
+    ]
+    bypass = ["Metrics", "Logging", "AzureServices"]
+  }
+}
