04158a78f127548201b16c12aa22bf5800eb4f4c
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-02-22 17:04:59 +0100

    add network rules

--- jenkins.io.tf
+++ jenkins.io.tf
@@ -14,6 +14,23 @@ resource "azurerm_storage_account" "jenkins_io" {
   min_tls_version                   = "TLS1_2" # default value, needed for tfsec
   infrastructure_encryption_enabled = true
 
+  # Adding a network rule with `public_network_access_enabled` set to `true` (default) selects the option "Enabled from selected virtual networks and IP addresses"
+  network_rules {
+    default_action = "Deny"
+    ip_rules = flatten(
+      concat(
+        [for key, value in module.jenkins_infra_shared_data.admin_public_ips : value],
+      )
+    )
+    virtual_network_subnet_ids = [
+      data.azurerm_subnet.publick8s_tier.id,
+      data.azurerm_subnet.privatek8s_tier.id,
+      data.azurerm_subnet.trusted_ci_jenkins_io_ephemeral_agents.id,
+      data.azurerm_subnet.trusted_ci_jenkins_io_sponsorship_ephemeral_agents.id,
+    ]
+    bypass = ["Metrics", "Logging", "AzureServices"]
+  }
+
   tags = local.default_tags
 }
 
