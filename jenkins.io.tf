c019287c30945a8e11020efecf45801b598a448f
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-02-22 16:08:30 +0100

    feat(jenkins.io): new premium storage account and infra.ci service principal writer

---
+++ jenkins.io.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "jenkins_io" {
+  name     = "jenkinsio"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "jenkins_io" {
+  name                     = "jenkinsio"
+  resource_group_name      = azurerm_resource_group.jenkins_io.name
+  location                 = azurerm_resource_group.jenkins_io.location
+  account_tier             = "Premium"
+  account_replication_type = "LRS"
+
+  tags = local.default_tags
+}
+
+resource "azurerm_storage_share" "jenkins_io" {
+  name                 = "jenkins-io"
+  storage_account_name = azurerm_storage_account.contributors_jenkins_io.name
+  quota                = 5 # Used capacity end of February 2024: 380Mio
+}
