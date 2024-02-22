14330c8d7ebe3cbb178b180e08449edb280bc374
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2024-02-22 18:16:11 +0100

    feat(jenkins.io): new premium storage and infra.ci service principal writer (#623)
    This PR adds a new Premium storage account and file share for
    jenkins.io, and a service principal writer allowing infra.ci.jenkins.io
    to use azcopy and short lived SAS token to interact with it.
    
    Looking at existing jenkins.io storage account, we noticed that most of
    its $70/month cost came from transactions.
    Using a Premium storage account instead with no transaction cost should
    allow us to decrease the monthly charge.
    
    Compared to the existing "prodjenkinsio" storage account, this PR sets
    the new storage account replication type from "GRS" to "ZRS", sufficient
    for our use case.
    
    Ref:
    - https://github.com/jenkins-infra/helpdesk/issues/3414
    
    ---------
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ jenkins.io.tf
@@ -0,0 +1,40 @@
+resource "azurerm_resource_group" "jenkins_io" {
+  name     = "jenkinsio"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "jenkins_io" {
+  name                              = "jenkinsio"
+  resource_group_name               = azurerm_resource_group.jenkins_io.name
+  location                          = azurerm_resource_group.jenkins_io.location
+  account_tier                      = "Premium"
+  account_kind                      = "FileStorage"
+  access_tier                       = "Hot"
+  account_replication_type          = "ZRS"
+  min_tls_version                   = "TLS1_2" # default value, needed for tfsec
+  infrastructure_encryption_enabled = true
+
+  # Adding a network rule with `public_network_access_enabled` set to `true` (default) selects the option "Enabled from selected virtual networks and IP addresses"
+  network_rules {
+    default_action = "Deny"
+    ip_rules = flatten(
+      concat(
+        [for key, value in module.jenkins_infra_shared_data.admin_public_ips : value],
+      )
+    )
+    virtual_network_subnet_ids = [
+      data.azurerm_subnet.publick8s_tier.id,
+      data.azurerm_subnet.trusted_ci_jenkins_io_ephemeral_agents.id,
+      data.azurerm_subnet.trusted_ci_jenkins_io_sponsorship_ephemeral_agents.id,
+    ]
+    bypass = ["Metrics", "Logging", "AzureServices"]
+  }
+
+  tags = local.default_tags
+}
+
+resource "azurerm_storage_share" "jenkins_io" {
+  name                 = "jenkins-io"
+  storage_account_name = azurerm_storage_account.contributors_jenkins_io.name
+  quota                = 5 # Used capacity end of February 2024: 380Mio
+}
