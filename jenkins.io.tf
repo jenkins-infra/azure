b10e8515e14d69ee5fa8158875c934ce5ad0fc86
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-02-22 16:36:09 +0100

    add missing parameters

--- jenkins.io.tf
+++ jenkins.io.tf
@@ -4,11 +4,15 @@ resource "azurerm_resource_group" "jenkins_io" {
 }
 
 resource "azurerm_storage_account" "jenkins_io" {
-  name                     = "jenkinsio"
-  resource_group_name      = azurerm_resource_group.jenkins_io.name
-  location                 = azurerm_resource_group.jenkins_io.location
-  account_tier             = "Premium"
-  account_replication_type = "ZRS"
+  name                              = "jenkinsio"
+  resource_group_name               = azurerm_resource_group.jenkins_io.name
+  location                          = azurerm_resource_group.jenkins_io.location
+  account_tier                      = "Premium"
+  account_kind                      = "FileStorage"
+  access_tier                       = "Hot"
+  account_replication_type          = "ZRS"
+  min_tls_version                   = "TLS1_2" # default value, needed for tfsec
+  infrastructure_encryption_enabled = true
 
   tags = local.default_tags
 }
