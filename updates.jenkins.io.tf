78572df9a9c51c8d4244fb20f21a7453750a97ff
smerle33 <stephane.merle+jenkins@gmail.com>
2024-05-28 14:41:09 +0200

    feat(cloudflare): add cname for the eastamerica bucket

--- updates.jenkins.io.tf
+++ updates.jenkins.io.tf
@@ -104,3 +104,13 @@ resource "azurerm_dns_ns_record" "updates_jenkins_io_cloudflare_zone_westeurope"
   records = ["jaxson.ns.cloudflare.com", "mira.ns.cloudflare.com"]
   tags    = local.default_tags
 }
+# East US
+resource "azurerm_dns_ns_record" "updates_jenkins_io_cloudflare_zone_eastamerica" {
+  name                = "eastamerica.cloudflare"
+  zone_name           = data.azurerm_dns_zone.jenkinsio.name
+  resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
+  ttl                 = 60
+  # Should correspond to the "zones_name_servers" output defined in https://github.com/jenkins-infra/cloudflare/blob/main/updates.jenkins.io.tf
+  records = ["jaxson.ns.cloudflare.com", "mira.ns.cloudflare.com"]
+  tags    = local.default_tags
+}
