80259f04f7b23ea9a859e85783fa67388ee0d869
smerle33 <stephane.merle+jenkins@gmail.com>
2024-06-18 10:30:54 +0200

    chore(privatek8s): terraform fmt

--- privatek8s.tf
+++ privatek8s.tf
@@ -124,8 +124,8 @@ resource "azurerm_kubernetes_cluster_node_pool" "linuxpool" {
 }
 
 resource "azurerm_kubernetes_cluster_node_pool" "infracipool" {
-  name    = "infracipool"
-  vm_size = "Standard_D8s_v3"
+  name                  = "infracipool"
+  vm_size               = "Standard_D8s_v3"
   os_disk_type          = "Ephemeral"
   os_disk_size_gb       = 200 # Ref. Cache storage size at https://learn.microsoft.com/en-us/azure/virtual-machines/dv3-dsv3-series#dsv3-series (depends on the instance size)
   orchestrator_version  = local.kubernetes_versions["privatek8s"]
@@ -157,8 +157,8 @@ resource "azurerm_kubernetes_cluster_node_pool" "infracipool" {
 }
 
 resource "azurerm_kubernetes_cluster_node_pool" "infraciarm64" {
-  name    = "arm64small"
-  vm_size = "Standard_D4pds_v5" # 4 vCPU, 16 GB RAM, local disk: 150 GB and 19000 IOPS
+  name                  = "arm64small"
+  vm_size               = "Standard_D4pds_v5" # 4 vCPU, 16 GB RAM, local disk: 150 GB and 19000 IOPS
   os_disk_type          = "Ephemeral"
   os_disk_size_gb       = 150 # Ref. Cache storage size at https://learn.microsoft.com/en-us/azure/virtual-machines/dpsv5-dpdsv5-series#dpdsv5-series (depends on the instance size)
   orchestrator_version  = local.kubernetes_versions["privatek8s"]
