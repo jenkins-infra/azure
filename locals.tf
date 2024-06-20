1985c1e50adcdf03b5f3c187a388e538017b168f
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2024-06-20 23:05:16 +0200

    chore: move `infra.ci.jenkins.io.tf` service principal end dates to `local.tf` (#734)

--- locals.tf
+++ locals.tf
@@ -1,3 +1,7 @@
+data "local_file" "locals_yaml" {
+  filename = "locals.yaml"
+}
+
 locals {
   subscription_main      = "dff2ec18-6a8e-405c-8e45-b7df7465acf0"
   subscription_sponsored = "1311c09f-aee0-4d6c-99a4-392c2b543204"
@@ -59,4 +63,6 @@ locals {
 
   weekly_ci_disk_size    = 8
   weekly_ci_access_modes = ["ReadWriteOnce"]
+
+  end_dates = yamldecode(data.local_file.locals_yaml.content).end_dates
 }
