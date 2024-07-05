19e1c7e277c29f7f14361f92cc034428e3a84b2f
Damien Duportal <damien.duportal@gmail.com>
2024-07-05 11:10:04 +0200

    feat(ldap,repo.azure) add new managed disk for AKS PV to support `arm64` migration
    - Use ZRS to allow moving across AZs
    - Decrease disk type from (overpriced) Premium to Standard SSD
    - Adapt disk size to their real production usage AND pricing model
        - Always less than 80 % usage to keep good performances
        - Use as much as available space as provided by Azure disks
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- ldap.jenkins.io.tf
+++ ldap.jenkins.io.tf
@@ -4,6 +4,77 @@ resource "azurerm_resource_group" "ldap" {
   tags     = local.default_tags
 }
 
+## LDAP uses the following data disk for its `/var/lib/ldap` data directory
+resource "azurerm_managed_disk" "ldap_jenkins_io_data" {
+  name                = "ldap-jenkins-io-data"
+  location            = azurerm_resource_group.ldap.location
+  resource_group_name = azurerm_resource_group.ldap.name
+  # ZRS to ensure we can move service across AZs
+  # Standard because it is enough for LDAP's IOPS and I/O bandwidth
+  # Ref. https://azure.microsoft.com/en-us/pricing/details/managed-disks/
+  storage_account_type = "StandardSSD_ZRS"
+  create_option        = "Empty"
+  # LDAP data set is between 300 and 500 Mb
+  # Class E1 (4G) only allow 7800 paid transactions per hour, while LDAP may peak at 8500 sometimes so E2 it is
+  # Ref. https://azure.microsoft.com/en-us/pricing/details/managed-disks/
+  disk_size_gb = 8
+  tags         = local.default_tags
+}
+resource "kubernetes_persistent_volume" "ldap_jenkins_io_data" {
+  provider = kubernetes.publick8s
+  metadata {
+    name = "ldap-jenkins-io-pv"
+  }
+  spec {
+    capacity = {
+      storage = azurerm_managed_disk.ldap_jenkins_io_data.disk_size_gb
+    }
+    access_modes                     = ["ReadWriteOnce"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = kubernetes_storage_class.statically_provisionned_publick8s.id
+    persistent_volume_source {
+      csi {
+        driver        = "disk.csi.azure.com"
+        volume_handle = azurerm_managed_disk.ldap_jenkins_io_data.id
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "ldap_jenkins_io_data" {
+  provider = kubernetes.publick8s
+  metadata {
+    name      = "ldap-jenkins-io-data"
+    namespace = "ldap"
+  }
+  spec {
+    access_modes       = kubernetes_persistent_volume.ldap_jenkins_io_data.spec[0].access_modes
+    volume_name        = kubernetes_persistent_volume.ldap_jenkins_io_data.metadata.0.name
+    storage_class_name = kubernetes_storage_class.statically_provisionned_publick8s.id
+    resources {
+      requests = {
+        storage = azurerm_managed_disk.ldap_jenkins_io_data.disk_size_gb
+      }
+    }
+  }
+}
+
+# Required to allow AKS CSI driver to access the Azure disk
+resource "azurerm_role_definition" "ldap_jenkins_io_controller_disk_reader" {
+  name  = "ReadLDAPDisk"
+  scope = azurerm_resource_group.ldap.id
+
+  permissions {
+    actions = ["Microsoft.Compute/disks/read"]
+  }
+}
+resource "azurerm_role_assignment" "ldap_jenkins_io_allow_azurerm" {
+  scope              = azurerm_resource_group.ldap.id
+  role_definition_id = azurerm_role_definition.ldap_jenkins_io_controller_disk_reader.role_definition_resource_id
+  principal_id       = azurerm_kubernetes_cluster.publick8s.identity[0].principal_id
+}
+
+## LDAP is backed-up (at regular intervals and on stopping) by a side container into the following Azure file storage
 resource "azurerm_storage_account" "ldap_backups" {
   name                     = "ldapjenkinsiobackups"
   resource_group_name      = azurerm_resource_group.ldap.name
@@ -16,7 +87,6 @@ resource "azurerm_storage_account" "ldap_backups" {
 
   tags = local.default_tags
 }
-
 resource "azurerm_storage_account_network_rules" "ldap_access" {
   storage_account_id = azurerm_storage_account.ldap_backups.id
 
@@ -33,14 +103,12 @@ resource "azurerm_storage_account_network_rules" "ldap_access" {
   # Grant access to trusted Azure Services like Azure Backup (see # https://learn.microsoft.com/en-gb/azure/storage/common/storage-network-security?tabs=azure-portal#exceptions)
   bypass = ["AzureServices"]
 }
-
 # TODO: find out how to create this without the 403 error encountered in #394, #396 & #398
 # resource "azurerm_storage_share" "ldap" {
 #   name                 = "ldap"
 #   storage_account_name = azurerm_storage_account.ldap_backups.name
 #   quota                = 5120 # 5To
 # }
-
 output "ldap_backups_primary_access_key" {
   value     = azurerm_storage_account.ldap_backups.primary_access_key
   sensitive = true
