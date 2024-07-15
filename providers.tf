301d2743415a6d6d90f86654898734c944dad7f7
Damien Duportal <damien.duportal@gmail.com>
2024-07-15 15:09:10 +0200

    chore: fix Terraform azuread provider errors during terratest execution
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  subscription_id            = local.subscription_main
+  subscription_id            = "dff2ec18-6a8e-405c-8e45-b7df7465acf0"
   skip_provider_registration = "true"
   features {}
 }
 provider "azurerm" {
   alias                      = "jenkins-sponsorship"
-  subscription_id            = local.subscription_sponsored
+  subscription_id            = "1311c09f-aee0-4d6c-99a4-392c2b543204"
   skip_provider_registration = "true"
   features {}
 }
