f140087e93f020f6fc1b835a37fd8914155db606
Damien Duportal <damien.duportal@gmail.com>
2024-08-09 20:05:16 +0200

    fix(outputs) we only need the DNS (first) record IP (#799)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 resource "local_file" "jenkins_infra_data_report" {
   content = jsonencode({
     "artifact-caching-proxy.privatelink.azurecr.io" = {
-      "service_ip" = azurerm_private_dns_a_record.artifact_caching_proxy,
+      "service_ip" = tolist(azurerm_private_dns_a_record.artifact_caching_proxy.records)[0],
     },
   })
   filename = "${path.module}/jenkins-infra-data-reports/azure.json"
