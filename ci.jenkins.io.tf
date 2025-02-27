1995f5f735b1c7524701dd6e2e551d244f755052
smerle33 <stephane.merle+jenkins@gmail.com>
2025-02-27 16:37:48 +0100

    more removal

--- ci.jenkins.io.tf
+++ ci.jenkins.io.tf
@@ -15,17 +15,3 @@ resource "azurerm_dns_cname_record" "assets_ci_jenkins_io" {
   record              = "assets.aws.ci.jenkins.io"
   tags                = local.default_tags
 }
-resource "azurerm_private_dns_a_record" "artifact_caching_proxy" {
-  provider            = azurerm.jenkins-sponsorship
-  name                = "artifact-caching-proxy"
-  zone_name           = azurerm_private_dns_zone.dockerhub_mirror["cijenkinsio"].name
-  resource_group_name = azurerm_private_dns_zone.dockerhub_mirror["cijenkinsio"].resource_group_name
-  ttl                 = 60
-  records = [
-    # Let's specify an IP at the end of the range to have low probability of being used
-    cidrhost(
-      data.azurerm_subnet.ci_jenkins_io_ephemeral_agents_jenkins_sponsorship.address_prefixes[0],
-      -2,
-    )
-  ]
-}
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,5 @@
 resource "local_file" "jenkins_infra_data_report" {
   content = jsonencode({
-    "artifact-caching-proxy.privatelink.azurecr.io" = {
-      "service_ip" = tolist(azurerm_private_dns_a_record.artifact_caching_proxy.records)[0],
-    },
     "public_redis" = {
       "service_hostname" = azurerm_redis_cache.public_redis.hostname,
       "service_port"     = azurerm_redis_cache.public_redis.port,
