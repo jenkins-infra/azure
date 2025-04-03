e25f9a1be89c5c6fee8565b1f884793d279d410e
Damien Duportal <damien.duportal@gmail.com>
2025-04-03 11:28:12 +0200

    Revert "cleanup(azure/ci.jio): remove unused ressources migrate to AWS"
    This reverts commit c7ccf9e8501ac69e8b032a0aaca69b1b50115bb1.

--- locals.tf
+++ locals.tf
@@ -46,6 +46,10 @@ locals {
   admin_username = "jenkins-infra-team"
 
   aks_clusters = {
+    "cijenkinsio_agents_1" = {
+      name               = "cijenkinsio-agents-1",
+      kubernetes_version = "1.30.7",
+    }
     "infracijenkinsio_agents_1" = {
       name               = "infracijenkinsio-agents-1",
       kubernetes_version = "1.31.6",
@@ -87,7 +91,9 @@ locals {
     },
   }
 
-  ci_jenkins_io_fqdn = "ci.jenkins.io"
+  ci_jenkins_io_fqdn                 = "ci.jenkins.io"
+  cijenkinsio_agents_1_compute_zones = [1]
+  ci_jenkins_io_agents_1_pod_cidr    = "10.100.0.0/14" # 10.100.0.1 - 10.103.255.255
 
   end_dates = yamldecode(data.local_file.locals_yaml.content).end_dates
 }
