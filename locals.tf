e138f9614526f8d90b4e0c14ec107a05817624ea
Damien Duportal <damien.duportal@gmail.com>
2025-04-04 19:06:05 +0200

    cleanup: remove unused or wrong items accidentally added back when reverting ci.jio (#998)
    Fixup of #986, caught by @smerle33 🥳 ❤️
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- locals.tf
+++ locals.tf
@@ -48,10 +48,6 @@ locals {
   admin_username = "jenkins-infra-team"
 
   aks_clusters = {
-    "cijenkinsio_agents_1" = {
-      name               = "cijenkinsio-agents-1",
-      kubernetes_version = "1.30.7",
-    }
     "infracijenkinsio_agents_1" = {
       name               = "infracijenkinsio-agents-1",
       kubernetes_version = "1.31.6",
@@ -93,9 +89,7 @@ locals {
     },
   }
 
-  ci_jenkins_io_fqdn                 = "ci.jenkins.io"
-  cijenkinsio_agents_1_compute_zones = [1]
-  ci_jenkins_io_agents_1_pod_cidr    = "10.100.0.0/14" # 10.100.0.1 - 10.103.255.255
+  ci_jenkins_io_fqdn = "ci.jenkins.io"
 
   end_dates = yamldecode(data.local_file.locals_yaml.content).end_dates
 }
