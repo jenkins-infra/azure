3d1307f2087e1afd1aee7a4cce76385c4176eb38
Jenkins Infra Bot (updatecli) <60776566+jenkins-infra-bot@users.noreply.github.com>
2025-04-07 12:52:15 +0000

    chore: Update Terraform file `infra.ci.jenkins.io.tf` with new expira...
    ... tion date
    
    Made with ❤️️ by updatecli

--- infra.ci.jenkins.io.tf
+++ infra.ci.jenkins.io.tf
@@ -421,7 +421,7 @@ resource "azuread_service_principal" "updatecli_infra_ci_jenkins_io" {
 resource "azuread_application_password" "updatecli_infra_ci_jenkins_io" {
   application_id = azuread_application.updatecli_infra_ci_jenkins_io.id
   display_name   = "updatecli_infra.ci.jenkins.io-tf-managed"
-  end_date       = "2025-04-18T00:00:00Z"
+  end_date       = "2025-07-06T00:00:00Z"
 }
 
 resource "azurerm_role_definition" "vm_images_reader" {
