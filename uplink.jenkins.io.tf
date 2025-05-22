8f657ef8ec863e88ca7289fe628b995dd702119e
Damien Duportal <damien.duportal@gmail.com>
2025-05-22 13:09:52 +0200

    feat(uplink) migrate database to public-db instance
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- uplink.jenkins.io.tf
+++ uplink.jenkins.io.tf
@@ -1,48 +1,24 @@
-
-removed {
-  from = postgresql_database.uplink
-
-  lifecycle {
-    destroy = false
-  }
+resource "postgresql_database" "uplink" {
+  name  = "uplink"
+  owner = postgresql_role.uplink.name
 }
-# resource "postgresql_database" "uplink" {
-#   name  = "uplink"
-#   owner = postgresql_role.uplink.name
-# }
-
-removed {
-  from = random_password.pgsql_uplink_user_password
-
-  lifecycle {
-    destroy = false
-  }
+resource "random_password" "pgsql_uplink_user_password" {
+  length           = 24
+  override_special = "!#%&*()-_=+[]{}:?"
+  special          = true
 }
-# resource "random_password" "pgsql_uplink_user_password" {
-#   length           = 24
-#   override_special = "!#%&*()-_=+[]{}:?"
-#   special          = true
-# }
-
-removed {
-  from = postgresql_role.uplink
-
-  lifecycle {
-    destroy = false
-  }
+resource "postgresql_role" "uplink" {
+  name     = "uplinkadmin"
+  login    = true
+  password = random_password.pgsql_uplink_user_password.result
 }
-# resource "postgresql_role" "uplink" {
-#   name     = "uplinkadmin"
-#   login    = true
-#   password = random_password.pgsql_uplink_user_password.result
-# }
 
-# # This (sensitive) output is meant to be encrypted into the production secret system, to be provided as a secret to the uplink.jenkins.io application
-# output "uplink_dbconfig" {
-#   sensitive   = true
-#   description = "YAML (secret) values for the Helm chart jenkins-infra/uplink"
-#   value       = <<-EOT
-# postgresql:
-#     url: postgres://${postgresql_role.uplink.name}:${random_password.pgsql_uplink_user_password.result}@${azurerm_postgresql_flexible_server.public_db.fqdn}:5432/${postgresql_database.uplink.name}
-#   EOT
-# }
+# This (sensitive) output is meant to be encrypted into the production secret system, to be provided as a secret to the uplink.jenkins.io application
+output "uplink_dbconfig" {
+  sensitive   = true
+  description = "YAML (secret) values for the Helm chart jenkins-infra/uplink"
+  value       = <<-EOT
+postgresql:
+    url: postgres://${postgresql_role.uplink.name}:${random_password.pgsql_uplink_user_password.result}@${azurerm_postgresql_flexible_server.public_db.fqdn}:5432/${postgresql_database.uplink.name}
+  EOT
+}
