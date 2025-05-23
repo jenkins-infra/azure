2b7f4e582c1b36fe770bd295dbf64abac43651eb
Damien Duportal <damien.duportal@gmail.com>
2025-05-23 14:02:33 +0200

    hotfix(uplink) rotate postgres password with no special char. - https://github.com/jenkins-infra/helpdesk/issues/4667#issuecomment-2904163274
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- uplink.jenkins.io.tf
+++ uplink.jenkins.io.tf
@@ -3,9 +3,8 @@ resource "postgresql_database" "uplink" {
   owner = postgresql_role.uplink.name
 }
 resource "random_password" "pgsql_uplink_user_password" {
-  length           = 24
-  override_special = "!#%&*()-_=+[]{}:?"
-  special          = true
+  length  = 24
+  special = false
 }
 resource "postgresql_role" "uplink" {
   name     = "uplinkadmin"
