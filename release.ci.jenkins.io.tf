1886efb6d0b24f2a46bee626421a91376a3fa5e6
smerle33 <stephane.merle+jenkins@gmail.com>
2025-07-03 14:43:52 +0200

    wip

--- release.ci.jenkins.io.tf
+++ release.ci.jenkins.io.tf
@@ -34,3 +34,15 @@ resource "azurerm_role_assignment" "release_ci_jenkins_io_controller_sponsorship
   role_definition_id = azurerm_role_definition.release_ci_jenkins_io_controller_sponsorship_disk_reader.role_definition_resource_id
   principal_id       = azurerm_kubernetes_cluster.privatek8s_sponsorship.identity[0].principal_id
 }
+
+resource "azurerm_user_assigned_identity" "release_ci_jenkins_io_agents" {
+  location            = var.location
+  name                = "release-ci-jenkins-io-agents"
+  resource_group_name = azurerm_resource_group.release_ci_jenkins_io_controller_jenkins_sponsorship.name
+}
+resource "azurerm_role_assignment" "release_ci_jenkins_io_azurevm_agents_write_buildsreports_share" {
+  scope = azurerm_storage_account.builds_reports_jenkins_io.id
+  # Allow writing
+  role_definition_name = "Storage File Data Privileged Contributor"
+  principal_id         = azurerm_user_assigned_identity.release_ci_jenkins_io_agents.principal_id
+}
