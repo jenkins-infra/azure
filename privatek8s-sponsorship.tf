a6a1d78e86b1e57ca3c7035925ea464c8a5592a7
Damien Duportal <damien.duportal@gmail.com>
2025-07-08 12:26:46 +0200

    hotfix(privatek8s/release.ci) use cluster RG and subscription for agents identity
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- privatek8s-sponsorship.tf
+++ privatek8s-sponsorship.tf
@@ -541,7 +541,7 @@ resource "kubernetes_service_account" "privatek8s_sponsorship_jenkins_release_ag
 resource "azurerm_federated_identity_credential" "privatek8s_sponsorship_jenkins_release_agents" {
   provider            = azurerm.jenkins-sponsorship
   name                = "privatek8s-sponsorship-${kubernetes_service_account.privatek8s_sponsorship_jenkins_release_agents.metadata[0].name}"
-  resource_group_name = azurerm_resource_group.release_ci_jenkins_io_controller_jenkins_sponsorship.name
+  resource_group_name = azurerm_kubernetes_cluster.privatek8s_sponsorship.resource_group_name
   audience            = ["api://AzureADTokenExchange"]
   issuer              = azurerm_kubernetes_cluster.privatek8s_sponsorship.oidc_issuer_url
   parent_id           = azurerm_user_assigned_identity.release_ci_jenkins_io_agents.id
