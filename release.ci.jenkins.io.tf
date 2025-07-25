985a51ec679cc0f8b09421651541f1621df15ae1
Damien Duportal <damien.duportal@gmail.com>
2025-07-25 16:54:18 +0200

    chore(release.ci/trusted.ci) rename resources and cleanup unused 'moved' blocks
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- release.ci.jenkins.io.tf
+++ release.ci.jenkins.io.tf
@@ -1,3 +1,4 @@
+#### TODO: remove resources below when cleaning up for https://github.com/jenkins-infra/helpdesk/issues/4690
 resource "azurerm_resource_group" "release_ci_jenkins_io_controller_jenkins_sponsorship" {
   provider = azurerm.jenkins-sponsorship
   name     = "release-ci-jenkins-io-controller"
@@ -34,16 +35,24 @@ resource "azurerm_role_assignment" "release_ci_jenkins_io_controller_sponsorship
   role_definition_id = azurerm_role_definition.release_ci_jenkins_io_controller_sponsorship_disk_reader.role_definition_resource_id
   principal_id       = azurerm_kubernetes_cluster.privatek8s_sponsorship.identity[0].principal_id
 }
-
-resource "azurerm_user_assigned_identity" "release_ci_jenkins_io_agents" {
+moved {
+  from = azurerm_user_assigned_identity.release_ci_jenkins_io_agents
+  to   = azurerm_user_assigned_identity.release_ci_jenkins_io_agents_sponsorship
+}
+resource "azurerm_user_assigned_identity" "release_ci_jenkins_io_agents_sponsorship" {
   provider            = azurerm.jenkins-sponsorship
   location            = var.location
   name                = "release-ci-jenkins-io-agents"
   resource_group_name = azurerm_kubernetes_cluster.privatek8s_sponsorship.resource_group_name
 }
-resource "azurerm_role_assignment" "release_ci_jenkins_io_azurevm_agents_write_buildsreports_share" {
+moved {
+  from = azurerm_role_assignment.release_ci_jenkins_io_azurevm_agents_write_buildsreports_share
+  to   = azurerm_role_assignment.release_ci_jenkins_io_azurevm_agents_write_buildsreports_share_sponsorship
+}
+resource "azurerm_role_assignment" "release_ci_jenkins_io_azurevm_agents_write_buildsreports_share_sponsorship" {
   scope = azurerm_storage_account.builds_reports_jenkins_io.id
   # Allow writing
   role_definition_name = "Storage File Data Privileged Contributor"
-  principal_id         = azurerm_user_assigned_identity.release_ci_jenkins_io_agents.principal_id
+  principal_id         = azurerm_user_assigned_identity.release_ci_jenkins_io_agents_sponsorship.principal_id
 }
+####
