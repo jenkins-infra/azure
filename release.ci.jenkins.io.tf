54acec5c49585ff5eed2f3944d79b352e1631bb1
Damien Duportal <damien.duportal@gmail.com>
2025-07-30 11:19:05 +0200

    feat(release.ci) create disk/PV/PVC for migration to CDF subscription (#1143)
    Ref. https://github.com/jenkins-infra/helpdesk/issues/4690
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- release.ci.jenkins.io.tf
+++ release.ci.jenkins.io.tf
@@ -47,6 +47,55 @@ resource "azurerm_role_assignment" "release_ci_jenkins_io_azurevm_agents_write_b
 }
 
 #### TODO: remove resources below when cleaning up for https://github.com/jenkins-infra/helpdesk/issues/4690
+resource "azurerm_managed_disk" "release_ci_jenkins_io_data_import" {
+  name                 = "release-ci-jenkins-io-data-import"
+  location             = azurerm_resource_group.release_ci_jenkins_io_controller.location
+  resource_group_name  = azurerm_resource_group.release_ci_jenkins_io_controller.name
+  storage_account_type = "StandardSSD_ZRS"
+  create_option        = "Copy"
+  source_resource_id   = "/subscriptions/1311c09f-aee0-4d6c-99a4-392c2b543204/resourceGroups/backup-sponsorhip/providers/Microsoft.Compute/snapshots/release.ci-data-20250730-09h12Z"
+  disk_size_gb         = 64
+  tags                 = local.default_tags
+}
+resource "kubernetes_persistent_volume" "privatek8s_release_ci_jenkins_io_data_import" {
+  provider = kubernetes.privatek8s
+
+  metadata {
+    name = "release-ci-jenkins-io-data-import"
+  }
+  spec {
+    capacity = {
+      storage = "${azurerm_managed_disk.release_ci_jenkins_io_data_import.disk_size_gb}Gi"
+    }
+    access_modes                     = ["ReadWriteOnce"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = kubernetes_storage_class.privatek8s_statically_provisioned.id
+    persistent_volume_source {
+      csi {
+        driver        = "disk.csi.azure.com"
+        volume_handle = azurerm_managed_disk.release_ci_jenkins_io_data_import.id
+      }
+    }
+  }
+}
+resource "kubernetes_persistent_volume_claim" "privatek8s_release_ci_jenkins_io_data_import" {
+  provider = kubernetes.privatek8s
+
+  metadata {
+    name      = "release-ci-jenkins-io-data-import"
+    namespace = kubernetes_namespace.privatek8s["release-ci-jenkins-io"].metadata.0.name
+  }
+  spec {
+    access_modes       = kubernetes_persistent_volume.privatek8s_release_ci_jenkins_io_data_import.spec.0.access_modes
+    volume_name        = kubernetes_persistent_volume.privatek8s_release_ci_jenkins_io_data_import.metadata.0.name
+    storage_class_name = kubernetes_persistent_volume.privatek8s_release_ci_jenkins_io_data_import.spec.0.storage_class_name
+    resources {
+      requests = {
+        storage = "${azurerm_managed_disk.release_ci_jenkins_io_data_import.disk_size_gb}Gi"
+      }
+    }
+  }
+}
 resource "azurerm_resource_group" "release_ci_jenkins_io_controller_jenkins_sponsorship" {
   provider = azurerm.jenkins-sponsorship
   name     = "release-ci-jenkins-io-controller"
