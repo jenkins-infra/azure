d69755b57082bbea0f15f6f0dff6cb12d10eba49
Damien Duportal <damien.duportal@gmail.com>
2025-08-12 09:42:05 +0200

    cleanup: remove jenkins-sponsorship provider (aka. sponsored subscription) (#1156)
    Ref. https://github.com/jenkins-infra/helpdesk/issues/4765
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- providers.tf
+++ providers.tf
@@ -4,12 +4,6 @@ provider "azurerm" {
   resource_provider_registrations = "none"
   features {}
 }
-provider "azurerm" {
-  alias                           = "jenkins-sponsorship"
-  subscription_id                 = "1311c09f-aee0-4d6c-99a4-392c2b543204"
-  resource_provider_registrations = "none"
-  features {}
-}
 
 provider "kubernetes" {
   alias                  = "privatek8s"
