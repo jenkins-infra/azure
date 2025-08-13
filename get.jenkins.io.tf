5f9cfb0ef59e9a518331a0b6a9742bc0a62dd8a1
Damien Duportal <damien.duportal@gmail.com>
2025-08-13 08:21:53 +0200

    feat(get.jenkins.io,updates.jenkins.io) create PV/PVCs in the new centralized NFS volume
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- get.jenkins.io.tf
+++ get.jenkins.io.tf
@@ -1,10 +1,79 @@
-# Storage account
 resource "azurerm_resource_group" "get_jenkins_io" {
   name     = "get-jenkins-io"
   location = var.location
   tags     = local.default_tags
 }
+import {
+  to       = kubernetes_namespace.get_jenkins_io
+  id       = "get-jenkins-io"
+  provider = kubernetes.publick8s
+}
+resource "kubernetes_namespace" "get_jenkins_io" {
+  provider = kubernetes.publick8s
 
+  metadata {
+    name = "get-jenkins-io"
+  }
+}
+resource "kubernetes_persistent_volume" "get_jenkins_io" {
+  provider = kubernetes.publick8s
+  metadata {
+    name = kubernetes_namespace.get_jenkins_io.metadata[0].name
+  }
+  spec {
+    capacity = {
+      storage = "${azurerm_storage_share.data_storage_jenkins_io.quota}Gi"
+    }
+    access_modes                     = ["ReadOnlyMany"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = kubernetes_storage_class.statically_provisioned_publick8s.id
+    mount_options = [
+      "nconnect=4", # Mandatory value (4) for Premium Azure File Share NFS 4.1. Increasing require using NetApp NFS instead ($$$)
+      "noresvport", # ref. https://linux.die.net/man/5/nfs
+      "actimeo=10", # Data is changed quite often
+      "cto",        # Ensure data consistency at the cost of slower I/O
+    ]
+    persistent_volume_source {
+      csi {
+        driver  = "file.csi.azure.com"
+        fs_type = "ext4"
+        # `volumeHandle` must be unique on the cluster for this volume
+        volume_handle = kubernetes_namespace.get_jenkins_io.metadata[0].name
+        read_only     = false
+        volume_attributes = {
+          protocol       = "nfs"
+          resourceGroup  = azurerm_storage_account.data_storage_jenkins_io.resource_group_name
+          shareName      = azurerm_storage_share.data_storage_jenkins_io.name
+          storageAccount = azurerm_storage_account.data_storage_jenkins_io.name
+        }
+        node_stage_secret_ref {
+          name      = kubernetes_secret.publick8s_data_storage_jenkins_io_storage_account.metadata[0].name
+          namespace = kubernetes_secret.publick8s_data_storage_jenkins_io_storage_account.metadata[0].namespace
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_persistent_volume_claim" "get_jenkins_io" {
+  provider = kubernetes.publick8s
+  metadata {
+    name      = kubernetes_persistent_volume.get_jenkins_io.metadata[0].name
+    namespace = kubernetes_namespace.get_jenkins_io.metadata[0].name
+  }
+  spec {
+    access_modes       = kubernetes_persistent_volume.get_jenkins_io.spec[0].access_modes
+    volume_name        = kubernetes_persistent_volume.get_jenkins_io.metadata[0].name
+    storage_class_name = kubernetes_persistent_volume.get_jenkins_io.spec[0].storage_class_name
+    resources {
+      requests = {
+        storage = kubernetes_persistent_volume.get_jenkins_io.spec[0].capacity.storage
+      }
+    }
+  }
+}
+
+
+#### TODO: remove when migrated to the common data storage NFS file share - https://github.com/jenkins-infra/helpdesk/issues/4767
 resource "azurerm_storage_account" "get_jenkins_io" {
   name                = "getjenkinsio"
   resource_group_name = azurerm_resource_group.get_jenkins_io.name
@@ -54,3 +123,4 @@ resource "azurerm_storage_share" "get_jenkins_io_website" {
   # Minimal size, 1.6GiB used in 2020
   quota = 100
 }
+##### End TODO: remove
