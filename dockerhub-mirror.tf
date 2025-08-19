60ef78b1c640d66a3053ab4043bb6dbe5f63714e
Damien Duportal <damien.duportal@gmail.com>
2025-08-19 08:29:13 +0200

    feat(dockerhub-mirror) enable access to the ACR from trusted.ci.jenkins.io through private endpoint
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- dockerhub-mirror.tf
+++ dockerhub-mirror.tf
@@ -39,6 +39,11 @@ locals {
       "vnet_id"   = data.azurerm_virtual_network.private.id,
       "rg_name"   = data.azurerm_resource_group.private.name,
     },
+    "trustedcijenkinsio" = {
+      "subnet_id" = data.azurerm_subnet.trusted_ci_jenkins_io_ephemeral_agents.id,
+      "vnet_id"   = data.azurerm_virtual_network.trusted_ci_jenkins_io.id
+      "rg_name"   = data.azurerm_virtual_network.trusted_ci_jenkins_io.resource_group_name
+    },
   }
 }
 
