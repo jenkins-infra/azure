e4ad917cc5248eb2d0de4f4b41cc7d5c209c8029
Damien Duportal <damien.duportal@gmail.com>
2025-09-22 08:38:02 +0200

    feat(publick8s) add custom PV/PVCs for all web services except www (#1199)
    Ref. https://github.com/jenkins-infra/helpdesk/issues/4617
    
    - Add PV/PVCs (and NS + secrets) in CSI to existing web services shared
    accounts
    - Create a new file share for reports.jenkins.io (start empty)
    
    Note: all these PV/PVCs are expected to move (soon) to the centralized
    NFS: https://github.com/jenkins-infra/helpdesk/issues/4767. For now,
    let's migrate web services to the new cluster with explicit PV/PVCs
    (instead of pod direct CSI mounts in the old `publick8s-endless-ghoul`
    cluster)
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- reports.jenkins.io.tf
+++ reports.jenkins.io.tf
@@ -1,3 +1,44 @@
+resource "azurerm_resource_group" "reports_jenkins_io" {
+  name     = "reports-jenkins-io"
+  location = var.location
+
+  tags = {
+    scope = "terraform-managed"
+  }
+}
+resource "azurerm_storage_account" "reports_jenkins_io" {
+  name                       = "reportsjenkinsio"
+  resource_group_name        = azurerm_resource_group.reports_jenkins_io.name
+  location                   = azurerm_resource_group.reports_jenkins_io.location
+  account_tier               = "Standard"
+  account_replication_type   = "ZRS"
+  account_kind               = "StorageV2"
+  https_traffic_only_enabled = true
+  min_tls_version            = "TLS1_2"
+
+  network_rules {
+    default_action = "Deny"
+    virtual_network_subnet_ids = concat(
+      [
+        # Required for using the resource
+        data.azurerm_subnet.publick8s_tier.id,
+        data.azurerm_subnet.publick8s.id,
+      ],
+      # Required for managing the resource
+      local.app_subnets["infra.ci.jenkins.io"].agents,
+    )
+    bypass = ["AzureServices"]
+  }
+
+  tags = local.default_tags
+}
+
+resource "azurerm_storage_share" "reports_jenkins_io" {
+  name               = "reports-jenkins-io"
+  storage_account_id = azurerm_storage_account.reports_jenkins_io.id
+  quota              = 5
+}
+
 ############# Legacy resources to be removed once migrated to the new resources below
 resource "azurerm_resource_group" "prod_reports" {
   name     = "prod-reports"
