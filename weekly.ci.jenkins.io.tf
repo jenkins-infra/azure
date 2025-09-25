17f63507b40aa7c1619510954de80f2b1462a19f
Damien Duportal <damien.duportal@gmail.com>
2025-09-25 14:56:40 +0200

    hotfix(weekly.ci) recreate disk used for migration from latest (2nd) snapshot
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- weekly.ci.jenkins.io.tf
+++ weekly.ci.jenkins.io.tf
@@ -12,13 +12,12 @@ resource "azurerm_managed_disk" "weekly_ci_jenkins_io" {
   tags                 = local.default_tags
 }
 
-
 ## TODO: remove resources below after migration
 resource "azurerm_managed_disk" "weekly_ci_jenkins_io_orig" {
   name                 = "weekly-ci-jenkins-io-orig"
   location             = azurerm_resource_group.weekly_ci_jenkins_io.location
   create_option        = "Copy"
-  source_resource_id   = "/subscriptions/dff2ec18-6a8e-405c-8e45-b7df7465acf0/resourceGroups/weekly-ci/providers/Microsoft.Compute/snapshots/weekly-ci-jenkins-io-20250924"
+  source_resource_id   = "/subscriptions/dff2ec18-6a8e-405c-8e45-b7df7465acf0/resourceGroups/weekly-ci/providers/Microsoft.Compute/snapshots/data-20250925"
   resource_group_name  = azurerm_resource_group.weekly_ci_jenkins_io.name
   storage_account_type = "StandardSSD_ZRS"
   disk_size_gb         = 8
