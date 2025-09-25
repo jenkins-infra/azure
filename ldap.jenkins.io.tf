b233865e2ab8f9c34a78925f8efc16632efb698e
Damien Duportal <damien.duportal@gmail.com>
2025-09-25 16:03:00 +0200

    cleanup(publick8s/ldap/weekly.ci) remove temporary volumes used for migration (#1211)
    Related to
    https://github.com/jenkins-infra/helpdesk/issues/4617#issuecomment-3332342227
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- ldap.jenkins.io.tf
+++ ldap.jenkins.io.tf
@@ -51,15 +51,3 @@ resource "azurerm_storage_share" "ldap_jenkins_io_backups" {
   # Unless this is a Premium Storage, we only pay for the storage we consume
   quota = 10
 }
-
-## TODO: remove resources below after migration
-resource "azurerm_managed_disk" "ldap_jenkins_io_data_orig" {
-  name                 = "ldap-jenkins-io-data-orig"
-  location             = azurerm_resource_group.ldap_jenkins_io.location
-  create_option        = "Copy"
-  source_resource_id   = "/subscriptions/dff2ec18-6a8e-405c-8e45-b7df7465acf0/resourceGroups/ldap/providers/Microsoft.Compute/snapshots/ldap-data-2025-0925"
-  resource_group_name  = azurerm_resource_group.ldap_jenkins_io.name
-  storage_account_type = "StandardSSD_ZRS"
-  disk_size_gb         = 8
-  tags                 = local.default_tags
-}
