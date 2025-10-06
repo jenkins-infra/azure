f36e10ee7245a784db1d23ddb2ce84dc6852c78a
Damien Duportal <damien.duportal@gmail.com>
2025-10-06 16:17:44 +0200

    cleanup(reports.jenkins.io) remove old (V1) storage system in favor of the V2 new one
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- reports.jenkins.io.tf
+++ reports.jenkins.io.tf
@@ -37,34 +37,3 @@ resource "azurerm_storage_share" "reports_jenkins_io" {
   storage_account_id = azurerm_storage_account.reports_jenkins_io.id
   quota              = 5
 }
-
-############# Legacy resources to be removed once migrated to the new resources below
-resource "azurerm_resource_group" "prod_reports" {
-  name     = "prod-reports"
-  location = var.location
-
-  tags = {
-    scope = "terraform-managed"
-  }
-}
-resource "azurerm_storage_account" "prodjenkinsreports" {
-  name                       = "prodjenkinsreports"
-  resource_group_name        = azurerm_resource_group.prod_reports.name
-  location                   = azurerm_resource_group.prod_reports.location
-  account_tier               = "Standard"
-  account_replication_type   = "GRS"
-  account_kind               = "Storage"
-  https_traffic_only_enabled = true
-  min_tls_version            = "TLS1_2"
-
-  tags = {
-    scope = "terraform-managed"
-  }
-}
-resource "azurerm_storage_share" "reports" {
-  name               = "reports"
-  storage_account_id = azurerm_storage_account.prodjenkinsreports.id
-  quota              = 1
-}
-
-############# End of legacy resources to be removed once migrated to the new resources below
