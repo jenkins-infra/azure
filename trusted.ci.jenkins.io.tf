016bca28fcf74791d9a52fa8502b0f592d5c2ffd
Damien Duportal <damien.duportal@gmail.com>
2025-10-29 07:22:31 +0100

    feat(trusted.ci/agent.trusted) migrate to the new VM
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- trusted.ci.jenkins.io.tf
+++ trusted.ci.jenkins.io.tf
@@ -51,7 +51,6 @@ module "trusted_ci_jenkins_io" {
   agent_ip_prefixes = concat(
     data.azurerm_subnet.trusted_ci_jenkins_io_ephemeral_agents.address_prefixes,
     [
-      azurerm_linux_virtual_machine.trusted_permanent_agent.private_ip_address,
       azurerm_linux_virtual_machine.agent_trusted_ci_jenkins_io.private_ip_address
     ],
   )
@@ -135,98 +134,8 @@ module "trustedci_reportsjenkinsio_fileshare_serviceprincipal_writer" {
 }
 
 ####################################################################################
-## Resources for the permanent agent VM
-####################################################################################
-resource "azurerm_resource_group" "trusted_ci_jenkins_io_permanent_agents" {
-  name     = "jenkinsinfra-trusted-permanent-agents"
-  location = var.location
-  tags     = local.default_tags
-}
-resource "azurerm_network_interface" "trusted_permanent_agent" {
-  name                = "agent.trusted.ci.jenkins.io"
-  location            = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.location
-  resource_group_name = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.name
-  tags                = local.default_tags
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.trusted_ci_jenkins_io_permanent_agents.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-resource "azurerm_linux_virtual_machine" "trusted_permanent_agent" {
-  name                            = "agent.trusted.ci.jenkins.io"
-  resource_group_name             = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.name
-  location                        = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.location
-  tags                            = local.default_tags
-  size                            = "Standard_D2as_v5"
-  admin_username                  = local.admin_username
-  disable_password_authentication = true
-  network_interface_ids = [
-    azurerm_network_interface.trusted_permanent_agent.id,
-  ]
-
-  admin_ssh_key {
-    username   = local.admin_username
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5K7Ro7jBl5Kc68RdzG6EXHstIBFSxO5Da8SQJSMeCbb4cHTYuBBH8jNsAFcnkN64kEu+YhmlxaWEVEIrPgfGfs13ZL7v9p+Nt76tsz6gnVdAy2zCz607pAWe7p4bBn6T9zdZcBSnvjawO+8t/5ue4ngcfAjanN5OsOgLeD6yqVyP8YTERjW78jvp2TFrIYmgWMI5ES1ln32PQmRZwc1eAOsyGJW/YIBdOxaSkZ41qUvb9b3dCorGuCovpSK2EeNphjLPpVX/NRpVY4YlDqAcTCdLdDrEeVqkiA/VDCYNhudZTDa8f1iHwBE/GEtlKmoO6dxJ5LAkRk3RIVHYrmI6XXSw5l0tHhW5D12MNwzUfDxQEzBpGK5iSfOBt5zJ5OiI9ftnsq/GV7vCXfvMVGDLUC551P5/s/wM70QmHwhlGQNLNeJxRTvd6tL11bof3K+29ivFYUmpU17iVxYOWhkNY86WyngHU6Ux0zaczF3H6H0tpg1Ca/cFO428AVPw/RTJpcAe6OVKq5zwARNApQ/p6fJKUAdXap+PpQGZlQhPLkUbwtFXGTrpX9ePTcdzryCYjgrZouvy4ZMzruJiIbFUH8mRY3xVREVaIsJakruvgw3b14oQgcB4BwYVBBqi62xIvbRzAv7Su9t2jK6OR2z3sM/hLJRqIJ5oILMORa7XqrQ== smerle@MacBook-Pro-de-Stephane.local"
-  }
-
-  user_data = base64encode(
-    templatefile("./.shared-tools/terraform/cloudinit.tftpl", {
-      hostname       = "agent.trusted.ci.jenkins.io",
-      admin_username = local.admin_username,
-      }
-  ))
-  computer_name = "agent.trusted.ci.jenkins.io"
-
-  # Encrypt all disks (ephemeral, temp dirs and data volumes) - https://learn.microsoft.com/en-us/azure/virtual-machines/disks-enable-host-based-encryption-portal?tabs=azure-powershell
-  encryption_at_host_enabled = true
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    disk_size_gb         = "128" # existing is 100GB, but as we will pay for 128GB, let's use it
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-minimal-jammy"
-    sku       = "minimal-22_04-lts-gen2"
-    version   = "latest"
-  }
-
-  identity {
-    type = "UserAssigned"
-    identity_ids = [
-      azurerm_user_assigned_identity.trusted_ci_jenkins_io_azurevm_agents_jenkins.id,
-    ]
-  }
-}
-resource "azurerm_managed_disk" "trusted_permanent_agent_data_disk" {
-  name                 = "trusted-permanent-agent-data-disk"
-  location             = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.location
-  resource_group_name  = azurerm_resource_group.trusted_ci_jenkins_io_permanent_agents.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "1024" # no intermediate between 512 and 1024
-
-  tags = local.default_tags
-}
-resource "azurerm_virtual_machine_data_disk_attachment" "trusted_permanent_agent_data_disk" {
-  managed_disk_id    = azurerm_managed_disk.trusted_permanent_agent_data_disk.id
-  virtual_machine_id = azurerm_linux_virtual_machine.trusted_permanent_agent.id
-  lun                = "20"
-  caching            = "ReadWrite"
-}
-
-####################################################################################
 ## Network Security Group and rules
 ####################################################################################
-resource "azurerm_subnet_network_security_group_association" "trusted_ci_permanent_agent" {
-  subnet_id                 = data.azurerm_subnet.trusted_ci_jenkins_io_permanent_agents.id
-  network_security_group_id = module.trusted_ci_jenkins_io.controller_nsg_id
-}
-
 ## Outbound Rules (different set of priorities than Inbound rules) ##
 resource "azurerm_network_security_rule" "allow_out_from_trusted_all_to_uc" {
   name              = "allow-out-from-trusted-all-to-uc"
@@ -276,25 +185,6 @@ resource "azurerm_network_security_rule" "allow_out_many_from_trusted_agents_to_
   resource_group_name         = module.trusted_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_rg_name
   network_security_group_name = module.trusted_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
 }
-# Ignore the rule as it does not detect the IP restriction to only update.jenkins.io"s host
-#trivy:ignore:azure-network-no-public-egress
-resource "azurerm_network_security_rule" "allow_outbound_ssh_from_permanent_agent_to_pkg" {
-  name                   = "allow-outbound-ssh-from-permanent-agent-to-pkg"
-  priority               = 4080
-  direction              = "Outbound"
-  access                 = "Allow"
-  protocol               = "Tcp"
-  source_port_range      = "*"
-  destination_port_range = "22"
-  source_address_prefixes = [
-    azurerm_linux_virtual_machine.trusted_permanent_agent.private_ip_address,
-    azurerm_linux_virtual_machine.agent_trusted_ci_jenkins_io.private_ip_address,
-  ]
-  destination_address_prefix  = local.external_services["pkg.origin.jenkins.io"]
-  resource_group_name         = module.trusted_ci_jenkins_io.controller_resourcegroup_name
-  network_security_group_name = module.trusted_ci_jenkins_io.controller_nsg_name
-}
-
 ## Inbound Rules (different set of priorities than Outbound rules) ##
 resource "azurerm_network_security_rule" "allow_in_many_from_trusted_agents_to_uc" {
   name              = "allow-in-many-from-trusted-agents-to-uc"
@@ -313,23 +203,6 @@ resource "azurerm_network_security_rule" "allow_in_many_from_trusted_agents_to_u
   ]
   resource_group_name         = module.trusted_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_rg_name
   network_security_group_name = module.trusted_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
-}
-
-resource "azurerm_network_security_rule" "allow_inbound_ssh_from_controller_to_permanent_agent" {
-  name                   = "allow-inbound-ssh-from-controller-to-permanent-agent"
-  priority               = 3600
-  direction              = "Inbound"
-  access                 = "Allow"
-  protocol               = "Tcp"
-  source_port_range      = "*"
-  destination_port_range = "22"
-  source_address_prefix  = module.trusted_ci_jenkins_io.controller_private_ipv4
-  destination_address_prefixes = [
-    azurerm_linux_virtual_machine.trusted_permanent_agent.private_ip_address,
-    azurerm_linux_virtual_machine.agent_trusted_ci_jenkins_io.private_ip_address,
-  ]
-  resource_group_name         = module.trusted_ci_jenkins_io.controller_resourcegroup_name
-  network_security_group_name = module.trusted_ci_jenkins_io.controller_nsg_name
 }
 
 ## Allow access to/from ACR endpoint
@@ -371,14 +244,6 @@ resource "azurerm_network_security_rule" "allow_in_https_from_trusted_to_acr" {
 ####################################################################################
 ## Public DNS records
 ####################################################################################
-resource "azurerm_dns_a_record" "trusted_permanent_agent" {
-  name                = "agent"
-  zone_name           = module.trusted_ci_jenkins_io_letsencrypt.zone_name
-  resource_group_name = data.azurerm_resource_group.proddns_jenkinsio.name
-  ttl                 = 60
-  # TODO: update
-  records = [azurerm_linux_virtual_machine.trusted_permanent_agent.private_ip_address]
-}
 resource "azurerm_dns_a_record" "trusted_ci_controller" {
   name                = "@"
   zone_name           = module.trusted_ci_jenkins_io_letsencrypt.zone_name
