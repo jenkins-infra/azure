1a3957e5f999fc39139cd85b3c0a5a6ac87f8031
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2025-12-08 17:44:22 +0100

    feat: allow Azure VM agents to access AKS clusters and Azure databases (#1264)
    This PR allows Azure VM agents to access AKS clusters and Azure
    databases so we can perform privatek8s Kubernetes upgrade from an
    ephemeral (VM) agent instead of a pod (which would be evicted).
    
    Required for:
    - #1263
    
    Ref:
    -
    https://github.com/jenkins-infra/helpdesk/issues/4820#issuecomment-3548723638

--- infra.ci.jenkins.io.tf
+++ infra.ci.jenkins.io.tf
@@ -175,6 +175,102 @@ resource "azurerm_network_security_rule" "allow_outbound_winrm_https_from_infrac
   network_security_group_name = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
 }
 
+# Allow infra.ci ephemeral agents to reach infracijenkinsio_agents_2 cluster
+resource "azurerm_network_security_rule" "allow_outbound_https_from_infraci_ephemeral_agents_to_infracijenkinsio_agents_2" {
+  name                    = "allow-outbound-https-from-infraci-agents-to-infracijenkinsio_agents-2"
+  priority                = 4084
+  direction               = "Outbound"
+  access                  = "Allow"
+  protocol                = "Tcp"
+  source_port_range       = "*"
+  destination_port_range  = "443"
+  source_address_prefixes = [
+    data.azurerm_subnet.infra_ci_jenkins_io_ephemeral_agents.address_prefix,
+    data.azurerm_subnet.infracijenkinsio_agents_2.address_prefix,
+  ]
+  # TODO: restrict to required resources only
+  destination_address_prefixes = [data.azurerm_subnet.infracijenkinsio_agents_2.address_prefix]
+  resource_group_name          = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_rg_name
+  network_security_group_name  = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
+}
+
+# Allow infra.ci ephemeral agents to reach privatek8s cluster
+resource "azurerm_network_security_rule" "allow_outbound_https_from_infraci_ephemeral_agents_to_privatek8s" {
+  name                    = "allow-outbound-https-from-infraci-agents-to-privatek8s"
+  priority                = 4085
+  direction               = "Outbound"
+  access                  = "Allow"
+  protocol                = "Tcp"
+  source_port_range       = "*"
+  destination_port_range  = "443"
+  source_address_prefixes = [
+    data.azurerm_subnet.infra_ci_jenkins_io_ephemeral_agents.address_prefix,
+    data.azurerm_subnet.infracijenkinsio_agents_2.address_prefix,
+  ]
+  # TODO: restrict to required resources only
+  destination_address_prefixes = [data.azurerm_subnet.privatek8s_tier.address_prefix]
+  resource_group_name          = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_rg_name
+  network_security_group_name  = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
+}
+
+# Allow infra.ci ephemeral agents to reach publick8s cluster
+resource "azurerm_network_security_rule" "allow_outbound_https_from_infraci_ephemeral_agents_to_publick8s" {
+  name                    = "allow-outbound-https-from-infraci-agents-to-publick8s"
+  priority                = 4086
+  direction               = "Outbound"
+  access                  = "Allow"
+  protocol                = "Tcp"
+  source_port_range       = "*"
+  destination_port_range  = "443"
+  source_address_prefixes = [
+    data.azurerm_subnet.infra_ci_jenkins_io_ephemeral_agents.address_prefix,
+    data.azurerm_subnet.infracijenkinsio_agents_2.address_prefix,
+  ]
+  # TODO: restrict to required resources only
+  # publick8s is a dual-stack cluster, filtering on IPv4 only
+  destination_address_prefixes = [
+    for ip in data.azurerm_subnet.publick8s.address_prefixes : ip if can(cidrnetmask(ip))
+  ]
+  resource_group_name          = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_rg_name
+  network_security_group_name  = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
+}
+
+# Allow infra.ci ephemeral agents to reach mysql-public-db hosted on Azure
+resource "azurerm_network_security_rule" "allow_outbound_mysql_from_infraci_ephemeral_agents_to_mysql_public_db" {
+  name                         = "allow-outbound-mysql-from-infraci-agents-to-mysql-public-db"
+  priority                     = 4087
+  direction                    = "Outbound"
+  access                       = "Allow"
+  protocol                     = "Tcp"
+  source_port_range            = "*"
+  destination_port_range       = "3306"
+  source_address_prefixes = [
+    data.azurerm_subnet.infra_ci_jenkins_io_ephemeral_agents.address_prefix,
+    data.azurerm_subnet.infracijenkinsio_agents_2.address_prefix,
+  ]
+  destination_address_prefixes = [data.azurerm_subnet.public_db_vnet_mysql_tier.address_prefix]
+  resource_group_name          = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_rg_name
+  network_security_group_name  = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
+}
+
+# Allow infra.ci ephemeral agents to reach postgres-public-db hosted on Azure
+resource "azurerm_network_security_rule" "allow_outbound_postgres_from_infraci_ephemeral_agents_to_postgres_public_db" {
+  name                         = "allow-outbound-postgres-from-infraci-agents-to-postgres-public-db"
+  priority                     = 4088
+  direction                    = "Outbound"
+  access                       = "Allow"
+  protocol                     = "Tcp"
+  source_port_range            = "*"
+  destination_port_range       = "5432"
+  source_address_prefixes = [
+    data.azurerm_subnet.infra_ci_jenkins_io_ephemeral_agents.address_prefix,
+    data.azurerm_subnet.infracijenkinsio_agents_2.address_prefix,
+  ]
+  destination_address_prefixes = [data.azurerm_subnet.public_db_vnet_postgres_tier.address_prefix]
+  resource_group_name          = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_rg_name
+  network_security_group_name  = module.infra_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
+}
+
 # Required to allow azcopy sync of plugins.jenkins.io File Share
 module "infraci_pluginsjenkinsio_fileshare_serviceprincipal_writer" {
   source = "./.shared-tools/terraform/modules/azure-jenkinsinfra-fileshare-serviceprincipal-writer"
