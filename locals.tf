99567f6befc43af502db9472032f2d6c7acd0ccf
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2025-12-12 09:58:19 +0100

    feat(privatek8s): upgrade to Kubernetes 1.33.5 (#1269)
    This PR upgrades `privatek8s` cluster to Kubernetes 1.33.5.
    
    Ref:
    -
    https://github.com/jenkins-infra/helpdesk/issues/4820#issuecomment-3548723638

--- locals.tf
+++ locals.tf
@@ -66,7 +66,7 @@ locals {
     },
     "privatek8s" = {
       name               = "privatek8s",
-      kubernetes_version = "1.32.7",
+      kubernetes_version = "1.33.5",
       # https://learn.microsoft.com/en-us/azure/aks/concepts-network-azure-cni-overlay#pods
       pod_cidr = "10.100.0.0/14", # 10.100.0.1 - 10.103.255.255
     },
