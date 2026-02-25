5d43da815037f17ac45c53298349a1bd34566bc6
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2026-02-25 11:53:10 +0100

    Revert "Revert "Revert "feat(cert.ci): add agents resources in the Jenkins Sp…"
    This reverts commit f11763900008cbcf8a4ab9ac9d68cb231e1617a5.

--- providers.tf
+++ providers.tf
@@ -1,13 +1,6 @@
-# Main Jenkins Subscription, paid by CDF
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   subscription_id                 = "dff2ec18-6a8e-405c-8e45-b7df7465acf0"
-  resource_provider_registrations = "none"
-  features {}
-}
-# "Pay as you go" Subscription - https://github.com/jenkins-infra/helpdesk/issues/5003
-provider "azurerm" {
-  alias                           = "jenkins-sponsored"
-  subscription_id                 = "1e7d5219-acbc-4495-8629-bdbb22e9b3ed"
   resource_provider_registrations = "none"
   features {}
 }
