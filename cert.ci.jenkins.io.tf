6a9f1d56d181514d2402a6f372a14f856dbf41ac
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-03-05 16:36:35 +0100

    chore(dockerhub-mirror): remove `certcijenkinsio` ACR private link

--- cert.ci.jenkins.io.tf
+++ cert.ci.jenkins.io.tf
@@ -112,44 +112,6 @@ resource "azurerm_dns_a_record" "assets_cert_ci_jenkins_io" {
   records             = [module.cert_ci_jenkins_io.controller_private_ipv4]
 }
 
-## Allow access to/from ACR endpoint
-resource "azurerm_network_security_rule" "allow_out_https_from_cert_agents_to_acr" {
-  count                   = var.terratest ? 0 : 1
-  name                    = "allow-out-https-from-agents-to-acr"
-  priority                = 4050
-  direction               = "Outbound"
-  access                  = "Allow"
-  protocol                = "Tcp"
-  source_port_range       = "*"
-  destination_port_range  = "443"
-  source_address_prefixes = data.azurerm_subnet.cert_ci_jenkins_io_ephemeral_agents.address_prefixes
-  destination_address_prefixes = distinct(
-    flatten(
-      [for rs in azurerm_private_endpoint.dockerhub_mirror["certcijenkinsio"].private_dns_zone_configs.*.record_sets : rs.*.ip_addresses]
-    )
-  )
-  resource_group_name         = module.cert_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_rg_name
-  network_security_group_name = module.cert_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
-}
-resource "azurerm_network_security_rule" "allow_in_https_from_cert_agents_to_acr" {
-  count                   = var.terratest ? 0 : 1
-  name                    = "allow-in-https-from-agents-to-acr"
-  priority                = 4050
-  direction               = "Inbound"
-  access                  = "Allow"
-  protocol                = "Tcp"
-  source_port_range       = "*"
-  destination_port_range  = "443"
-  source_address_prefixes = data.azurerm_subnet.cert_ci_jenkins_io_ephemeral_agents.address_prefixes
-  destination_address_prefixes = distinct(
-    flatten(
-      [for rs in azurerm_private_endpoint.dockerhub_mirror["certcijenkinsio"].private_dns_zone_configs.*.record_sets : rs.*.ip_addresses]
-    )
-  )
-  resource_group_name         = module.cert_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_rg_name
-  network_security_group_name = module.cert_ci_jenkins_io_azurevm_agents.ephemeral_agents_nsg_name
-}
-
 module "cert_ci_jenkins_io_letsencrypt" {
   source = "./modules/azure-letsencrypt-dns"
 
