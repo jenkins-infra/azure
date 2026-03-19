be71c3c582eca3fb18c934eb0bf6adfe20d0cf46
Damien Duportal <damien.duportal@gmail.com>
2026-03-19 16:54:33 +0100

    feat(packer,cert.ci, trusted.ci, infra.ci) create new empty VM image galleries (#1345)
    Ref. https://github.com/jenkins-infra/helpdesk/issues/5045
    
    Expecting 27 new resources:
    
    - 3 new RGs
    - 3 new galleries
    - 15 new VM definition (5 per gallery)
    - 3 new role assignments (1 per RG) for the packer Azure Client
    - 3 new role assignments to allow the 3 controllers to read the `prod`
    gallery
    
    Note: I willingly kept the 3 role assignments for controllers in the CDF
    subscription so they stay close to the controllers. But the
    Packer-directly related assignments and resources are in the new
    subscription
    
    ---------
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- packer-resources.tf
+++ packer-resources.tf
@@ -127,3 +127,92 @@ resource "azurerm_role_assignment" "packer_role_manage_subnet_cdf" {
   role_definition_name = "Network Contributor"
   principal_id         = azuread_service_principal.packer.object_id
 }
+
+##### Sponsored subscription
+
+## Dev Resources are used by the pull requests in jenkins-infra/packer-images
+## Staging Resources are used by the "main" branch builds
+## Prod Resources are used for final Packer artifacts
+resource "azurerm_resource_group" "packer_images_sponsored" {
+  provider = azurerm.jenkins-sponsored
+
+  for_each = local.shared_galleries
+
+  name     = "${each.key}-packer-images"
+  location = var.location
+}
+
+resource "azurerm_shared_image_gallery" "packer_images_sponsored" {
+  provider = azurerm.jenkins-sponsored
+
+  for_each = local.shared_galleries
+
+  # Note: dashes are not allowed ("name can only contain alphanumeric, full stops and underscores") unlike our usual naming convention
+  name                = "${each.key}_packer_images"
+  resource_group_name = azurerm_resource_group.packer_images_sponsored[each.key].name
+  location            = azurerm_resource_group.packer_images_sponsored[each.key].location
+  description         = each.value.description
+
+  tags = {
+    scope = "terraform-managed"
+  }
+}
+
+# Note that Terraform does NOT manage image versions (it's packer-based),
+# which requires to be manually deleted when cleaning up resources above.
+resource "azurerm_shared_image" "packer_images_sponsored" {
+  provider = azurerm.jenkins-sponsored
+
+  # Generate a list of images in the form "<gallery name>_<image_name>"
+  for_each = toset(
+    distinct(
+      flatten([
+        for gallery_key, gallery_value in local.shared_galleries : [
+          for image_key in gallery_value.images : "${gallery_key}_${image_key}"
+        ]
+      ])
+    )
+  )
+
+  name                = format("jenkins-agent-%s", split("_", each.value)[1])
+  gallery_name        = azurerm_shared_image_gallery.packer_images_sponsored[split("_", each.value)[0]].name
+  resource_group_name = azurerm_resource_group.packer_images_sponsored[split("_", each.value)[0]].name
+  location            = azurerm_resource_group.packer_images_sponsored[split("_", each.value)[0]].location
+
+  architecture = length(regexall(".+arm64", split("_", each.value)[1])) > 0 ? "Arm64" : "x64"
+
+  hyper_v_generation = "V2"
+  os_type            = length(regexall(".*windows.*", lower(split("_", each.value)[1]))) > 0 ? "Windows" : "Linux"
+  specialized        = false
+
+  ## v6/v7 instance generation requires NVMe controller and trusted_launch, while v5/v' require SCSCI
+  disk_controller_type_nvme_enabled = true
+  trusted_launch_supported          = true
+
+  lifecycle {
+    ignore_changes = [
+      eula, accelerated_network_support_enabled
+    ]
+  }
+
+  identifier {
+    publisher = format("jenkins-agent-%s", split("_", each.value)[1])
+    offer     = format("jenkins-agent-%s", split("_", each.value)[1])
+    sku       = format("jenkins-agent-%s", split("_", each.value)[1])
+  }
+
+  tags = {
+    scope = "terraform-managed"
+  }
+}
+
+# Allow packer Service Principal to manage AzureRM resources inside the packer resource groups
+resource "azurerm_role_assignment" "packer_images_sponsored" {
+  provider = azurerm.jenkins-sponsored
+
+  for_each = azurerm_resource_group.packer_images_sponsored
+
+  scope                = each.value.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.packer.object_id
+}
