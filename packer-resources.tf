08bf010584c8e95befa1fa47d4189c62ec638b60
Damien Duportal <damien.duportal@gmail.com>
2026-03-20 18:03:00 +0100

    cleanup(packer) remove galleries from CDF subscription (#1352)
    Ref. https://github.com/jenkins-infra/helpdesk/issues/5045
    
    Before merging this PR, we have to delete manually and recursively all
    VM image versions (yes :'( ).
    Might take some time:
    
    <img width="458" height="167" alt="Capture d’écran 2026-03-20 à 17 08
    53"
    src="https://github.com/user-attachments/assets/26733371-33bf-4770-8924-d08eca55a80e"
    />
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- packer-resources.tf
+++ packer-resources.tf
@@ -1,5 +1,4 @@
 # Azure Resources required or used by the repository jenkins-infra/packer-images
-
 resource "azuread_application" "packer" {
   display_name = "packer"
   owners = [
@@ -37,13 +36,6 @@ resource "azuread_application_password" "packer" {
 ## Dev Resources are used by the pull requests in jenkins-infra/packer-images
 ## Staging Resources are used by the "main" branch builds
 ## Prod Resources are used for final Packer artifacts
-resource "azurerm_resource_group" "packer_images_cdf" {
-  for_each = local.shared_galleries
-
-  name     = "${each.key}-packer-images"
-  location = data.azurerm_virtual_network.infra_ci_jenkins_io.location # Location of the packer subnet in infra.ci
-}
-
 resource "azurerm_resource_group" "packer_builds_cdf" {
   for_each = local.shared_galleries
 
@@ -51,69 +43,6 @@ resource "azurerm_resource_group" "packer_builds_cdf" {
   location = data.azurerm_virtual_network.infra_ci_jenkins_io.location # Location of the packer subnet in infra.ci
 }
 
-resource "azurerm_shared_image_gallery" "packer_images_cdf" {
-  for_each = local.shared_galleries
-
-  name                = "${each.key}_packer_images"
-  resource_group_name = azurerm_resource_group.packer_images_cdf[each.key].name
-  location            = data.azurerm_virtual_network.infra_ci_jenkins_io.location # Location of the packer subnet in infra.ci
-  description         = each.value.description
-
-  tags = {
-    scope = "terraform-managed"
-  }
-}
-
-# Note that Terraform does NOT manage image versions (it's packer-based).
-resource "azurerm_shared_image" "jenkins_agent_images_cdf" {
-  # Generate a list of images in the form "<gallery name>_<image_name>"
-  for_each = toset(
-    distinct(
-      flatten([
-        for gallery_key, gallery_value in local.shared_galleries : [
-          for image_key in gallery_value.images : "${gallery_key}_${image_key}"
-        ]
-      ])
-    )
-  )
-
-  name                = format("jenkins-agent-%s", split("_", each.value)[1])
-  gallery_name        = azurerm_shared_image_gallery.packer_images_cdf[split("_", each.value)[0]].name
-  resource_group_name = azurerm_resource_group.packer_images_cdf[split("_", each.value)[0]].name
-  location            = data.azurerm_virtual_network.infra_ci_jenkins_io.location # Location of the packer subnet in infra.ci
-
-  architecture = length(regexall(".+arm64", split("_", each.value)[1])) > 0 ? "Arm64" : "x64"
-
-  hyper_v_generation     = "V2"
-  os_type                = length(regexall(".*windows.*", lower(split("_", each.value)[1]))) > 0 ? "Windows" : "Linux"
-  specialized            = false
-  trusted_launch_enabled = false
-
-  lifecycle {
-    ignore_changes = [
-      eula, accelerated_network_support_enabled
-    ]
-  }
-
-  identifier {
-    publisher = format("jenkins-agent-%s", split("_", each.value)[1])
-    offer     = format("jenkins-agent-%s", split("_", each.value)[1])
-    sku       = format("jenkins-agent-%s", split("_", each.value)[1])
-  }
-
-  tags = {
-    scope = "terraform-managed"
-  }
-}
-
-# Allow packer Service Principal to manage AzureRM resources inside the packer resource groups
-resource "azurerm_role_assignment" "packer_role_images_assignement_cdf" {
-  for_each = azurerm_resource_group.packer_images_cdf
-
-  scope                = each.value.id
-  role_definition_name = "Contributor"
-  principal_id         = azuread_service_principal.packer.object_id
-}
 # Allow packer Service Principal to manage AzureRM resources inside the packer resource groups
 resource "azurerm_role_assignment" "packer_role_builds_assignement_cdf" {
   for_each = azurerm_resource_group.packer_builds_cdf
