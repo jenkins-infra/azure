7564e090940223a32d86ffe77d458b97519645d9
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2026-03-23 12:05:54 +0100

    cleanup(infra.ci): remove CDF ephemeral agents (#1353)
    Ref:
    - https://github.com/jenkins-infra/helpdesk/issues/5042

--- vnets.tf
+++ vnets.tf
@@ -87,11 +87,6 @@ data "azurerm_subnet" "private_vnet_data_tier" {
   virtual_network_name = data.azurerm_virtual_network.private.name
   resource_group_name  = data.azurerm_resource_group.private.name
 }
-data "azurerm_subnet" "infra_ci_jenkins_io_ephemeral_agents" {
-  name                 = "${data.azurerm_virtual_network.infra_ci_jenkins_io.name}-ephemeral-agents"
-  virtual_network_name = data.azurerm_virtual_network.infra_ci_jenkins_io.name
-  resource_group_name  = data.azurerm_virtual_network.infra_ci_jenkins_io.resource_group_name
-}
 data "azurerm_subnet" "infra_ci_jenkins_io_sponsored_ephemeral_agents" {
   provider             = azurerm.jenkins-sponsored
   name                 = "${data.azurerm_virtual_network.infra_ci_jenkins_io_sponsored.name}-ephemeral-agents"
