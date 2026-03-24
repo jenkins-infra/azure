118ef639fec9c0b150374ebefd57ea7ed849eb40
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-03-24 17:37:36 +0100

    hotfix(infracijenkinsio-agents-1): proper UAID
    Ref:
    - https://github.com/jenkins-infra/azure/pull/1361#issuecomment-4119708990

--- infraci.jenkins.io-agents-1.tf
+++ infraci.jenkins.io-agents-1.tf
@@ -158,7 +158,7 @@ resource "azurerm_federated_identity_credential" "infracijenkinsio_agents_1_infr
   name                      = "infracijenkinsio-agents-1-${kubernetes_service_account.infracijenkinsio_agents_1_infra_ci_jenkins_io_agents.metadata[0].name}"
   audience                  = ["api://AzureADTokenExchange"]
   issuer                    = azurerm_kubernetes_cluster.infracijenkinsio_agents_1.oidc_issuer_url
-  user_assigned_identity_id = azurerm_user_assigned_identity.infra_ci_jenkins_io_agents.id
+  user_assigned_identity_id = azurerm_user_assigned_identity.infra_ci_jenkins_io_agents_jenkins_sponsored.id
   subject                   = "system:serviceaccount:${kubernetes_namespace.infracijenkinsio_agents_1_infra_ci_jenkins_io_agents.metadata[0].name}:${kubernetes_service_account.infracijenkinsio_agents_1_infra_ci_jenkins_io_agents.metadata[0].name}"
 }
 
