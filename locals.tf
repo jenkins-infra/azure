8b80aeb3f53160d1eaae26160a6a2071bf4eb0be
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-03-27 19:11:05 +0100

    restore locals aks_clusters_outputs

--- locals.tf
+++ locals.tf
@@ -326,6 +326,9 @@ locals {
     "infracijenkinsio_agents_1" = {
       cluster_hostname = "https://${azurerm_kubernetes_cluster.infracijenkinsio_agents_1.fqdn}:443", # Cannot use the kubeconfig host as it provides a private DNS name
     },
+    "infracijenkinsio_agents_2" = {
+      cluster_hostname = "https://${azurerm_kubernetes_cluster.infracijenkinsio_agents_2.fqdn}:443", # Cannot use the kubeconfig host as it provides a private DNS name
+    },
     "privatek8s" = {
       cluster_hostname = "https://${azurerm_kubernetes_cluster.privatek8s.fqdn}:443", # Cannot use the kubeconfig host as it provides a private DNS name
     },
