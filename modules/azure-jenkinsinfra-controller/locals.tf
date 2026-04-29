40a0f5cc55f1dbb4015d1909fa69785f3032ce9b
Damien Duportal <damien.duportal@gmail.com>
2026-04-29 16:58:28 +0200

    feat(modules/azure-jenkinsinfra-controller) allow customization of the VM name/hostname with the optional variable 'controller_fqdn'
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- modules/azure-jenkinsinfra-controller/locals.tf
+++ modules/azure-jenkinsinfra-controller/locals.tf
@@ -3,6 +3,6 @@ locals {
   service_short_name          = trimprefix(trimprefix(var.service_fqdn, var.dns_zone), ".")
   service_short_stripped_name = replace(local.service_short_name, ".", "-")
   service_stripped_name       = replace(var.service_fqdn, ".", "-")
-  controller_fqdn             = "controller.${var.service_fqdn}"
+  controller_fqdn             = var.controller_fqdn == "" ? "controller.${var.service_fqdn}" : var.controller_fqdn
   controller_principal_id     = var.controller_service_principal_end_date == "" ? data.azurerm_virtual_machine.controller.identity[0].principal_id : azuread_service_principal.controller[0].object_id
 }
