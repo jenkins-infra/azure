fd3ff39800f022b2a9295bdc9fdd43a34c958f98
Damien Duportal <damien.duportal@gmail.com>
2026-05-04 13:58:23 +0200

    cleanup(trusted.ci.jenkins.io) remove controller resources from CDF subscription (#1441)
    Ref.
    https://github.com/jenkins-infra/helpdesk/issues/5084#issuecomment-4344122327
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- trusted.ci.jenkins.io.tf
+++ trusted.ci.jenkins.io.tf
@@ -9,56 +9,9 @@ module "trusted_ci_jenkins_io_letsencrypt" {
   dns_rg_name      = data.azurerm_resource_group.proddns_jenkinsio.name
   parent_zone_name = data.azurerm_dns_zone.jenkinsio.name
   principal_ids = [
-    module.trusted_ci_jenkins_io.controller_service_principal_id,
     module.trusted_ci_jenkins_io_sponsored.controller_service_principal_id,
   ]
 }
-module "trusted_ci_jenkins_io" {
-  source = "./modules/azure-jenkinsinfra-controller"
-
-  providers = {
-    azurerm     = azurerm
-    azurerm.dns = azurerm
-    azuread     = azuread
-  }
-
-  service_fqdn                 = "trusted.ci.jenkins.io"
-  location                     = data.azurerm_virtual_network.trusted_ci_jenkins_io.location
-  admin_username               = local.admin_username
-  admin_ssh_publickey          = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5K7Ro7jBl5Kc68RdzG6EXHstIBFSxO5Da8SQJSMeCbb4cHTYuBBH8jNsAFcnkN64kEu+YhmlxaWEVEIrPgfGfs13ZL7v9p+Nt76tsz6gnVdAy2zCz607pAWe7p4bBn6T9zdZcBSnvjawO+8t/5ue4ngcfAjanN5OsOgLeD6yqVyP8YTERjW78jvp2TFrIYmgWMI5ES1ln32PQmRZwc1eAOsyGJW/YIBdOxaSkZ41qUvb9b3dCorGuCovpSK2EeNphjLPpVX/NRpVY4YlDqAcTCdLdDrEeVqkiA/VDCYNhudZTDa8f1iHwBE/GEtlKmoO6dxJ5LAkRk3RIVHYrmI6XXSw5l0tHhW5D12MNwzUfDxQEzBpGK5iSfOBt5zJ5OiI9ftnsq/GV7vCXfvMVGDLUC551P5/s/wM70QmHwhlGQNLNeJxRTvd6tL11bof3K+29ivFYUmpU17iVxYOWhkNY86WyngHU6Ux0zaczF3H6H0tpg1Ca/cFO428AVPw/RTJpcAe6OVKq5zwARNApQ/p6fJKUAdXap+PpQGZlQhPLkUbwtFXGTrpX9ePTcdzryCYjgrZouvy4ZMzruJiIbFUH8mRY3xVREVaIsJakruvgw3b14oQgcB4BwYVBBqi62xIvbRzAv7Su9t2jK6OR2z3sM/hLJRqIJ5oILMORa7XqrQ=="
-  controller_network_name      = data.azurerm_virtual_network.trusted_ci_jenkins_io.name
-  controller_network_rg_name   = data.azurerm_resource_group.trusted_ci_jenkins_io.name
-  controller_subnet_name       = data.azurerm_subnet.trusted_ci_jenkins_io_controller.name
-  controller_data_disk_size_gb = 128
-  controller_vm_size           = "Standard_B2s"
-  default_tags                 = local.default_tags
-  dns_zone_name                = module.trusted_ci_jenkins_io_letsencrypt.zone_name
-  dns_resourcegroup_name       = module.trusted_ci_jenkins_io_letsencrypt.zone_rg_name
-
-  controller_resourcegroup_name = "jenkinsinfra-trusted-ci-controller"
-  controller_datadisk_name      = "trusted-ci-controller-data-disk"
-
-  jenkins_infra_ips = {
-    ldap_ipv4         = azurerm_public_ip.publick8s_ips["publick8s-ldap-ipv4"].ip_address,
-    puppet_ipv4       = azurerm_public_ip.puppet_jenkins_io.ip_address,
-    privatevpn_subnet = data.azurerm_subnet.private_vnet_data_tier.address_prefixes,
-  }
-
-  controller_service_principal_ids = [
-    # Commenting out to migrate to new AzureAD provider
-    # data.azuread_service_principal.terraform_production.id,
-    "b847a030-25e1-4791-ad04-9e8484d87bce",
-  ]
-  controller_packer_rg_ids = [
-    azurerm_resource_group.packer_images_sponsored["prod"].id,
-  ]
-
-  agent_ip_prefixes = concat(
-    data.azurerm_subnet.trusted_ci_jenkins_io_sponsored_ephemeral_agents.address_prefixes,
-    data.azurerm_subnet.trusted_ci_jenkins_io_sponsored_permanent_agents.address_prefixes,
-  )
-}
-
 ####################################################################################
 ## Resources for the Controller VM in the sponsored subscription
 ####################################################################################
