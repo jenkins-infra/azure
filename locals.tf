0843a0dc1900593809fba393e3da696e72c0b197
Damien Duportal <damien.duportal@gmail.com>
2026-06-01 22:47:24 +0200

    feat(privatek8s-sponsored) bump AKS to 1.34.7
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- locals.tf
+++ locals.tf
@@ -60,7 +60,7 @@ locals {
     },
     "privatek8s-sponsored" = {
       name               = "privatek8s-sponsored",
-      kubernetes_version = "1.33.11",
+      kubernetes_version = "1.34.7",
       # https://learn.microsoft.com/en-us/azure/aks/concepts-network-azure-cni-overlay#pods
       pod_cidr = "10.100.0.0/14", # 10.100.0.1 - 10.103.255.255
     },
