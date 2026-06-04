f57394ac574bc619aec5b1da908092fdf3eb415f
Damien Duportal <damien.duportal@gmail.com>
2026-06-04 14:53:23 +0200

    chore(backends) use clear-text backend config with only the `ARM_CLIENT_SECRET` as credential (#1492)
    Related to https://github.com/jenkins-infra/helpdesk/issues/5169
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- backend.tf
+++ backend.tf
@@ -1,4 +1,7 @@
 terraform {
   backend "azurerm" {
+    subscription_id = "dff2ec18-6a8e-405c-8e45-b7df7465acf0"
+    tenant_id       = "4c45fef2-1ba7-4120-80a0-9e2d03e9c2b6"
+    key             = "terraform.tfstate"
   }
 }
